8cd0f27caf7ab548eab836d76b8142a460909651
Heinz Gies <hgies@wayfair.com>
2018-08-15 17:53:38 +0200

    Multiple kafka consumers

--- src/input/kafka.rs
+++ src/input/kafka.rs
@@ -10,11 +10,12 @@ use rdkafka::Message;
 use rdkafka_sys;
 use serde_json;
 use std::collections::HashMap;
+use std::thread;
 
 use std::sync::mpsc;
 
 pub struct Input {
-    pub consumer: LoggingConsumer,
+    config: Config,
 }
 // A simple context to customize the consumer behavior and print a log line every time
 // offsets are committed
@@ -39,13 +40,18 @@ fn dflt_options() -> HashMap<String, String> {
     HashMap::new()
 }
 
-#[derive(Deserialize, Debug)]
+fn dflt_threads() -> usize {
+    1
+}
+#[derive(Deserialize, Debug, Clone)]
 struct Config {
     group_id: String,
     topics: Vec<String>,
     brokers: Vec<String>,
     #[serde(default = "dflt_options")]
     rdkafka_options: HashMap<String, String>,
+    #[serde(default = "dflt_threads")]
+    threads: usize,
 }
 // Define a new type for convenience
 pub type LoggingConsumer = StreamConsumer<LoggingConsumerContext>;
@@ -53,29 +59,7 @@ impl Input {
     pub fn new(opts: &str) -> Self {
         match serde_json::from_str(opts) {
             Ok(config @ Config { .. }) => {
-                info!(
-                    "Starting Kafka input: GroupID: {}, Topic: {:?}, Brokers: {:?}",
-                    config.group_id, config.topics, config.brokers
-                );
-                let context = LoggingConsumerContext;
-                let consumer: LoggingConsumer = config.rdkafka_options.iter().fold(&mut ClientConfig::new(), |c: &mut ClientConfig, (k, v)| c.set(k, v))
-                    .set("group.id", &config.group_id)
-                    .set("bootstrap.servers", &config.brokers.join(","))
-                    .set("enable.partition.eof", "false")
-                    .set("session.timeout.ms", "6000")
-                // Commit automatically every 5 seconds.
-                    .set("enable.auto.commit", "true")
-                    .set("auto.commit.interval.ms", "5000")
-                // but only commit the offsets explicitly stored via `consumer.store_offset`.
-                    .set("enable.auto.offset.store", "false")
-                    .set_log_level(RDKafkaLogLevel::Debug)
-                    .create_with_context(context)
-                    .expect("Consumer creation failed");
-                let topics: Vec<&str> = config.topics.iter().map(|topic| topic.as_str()).collect();
-                consumer
-                    .subscribe(&topics)
-                    .expect("Can't subscribe to specified topic");
-                Input { consumer }
+                Input { config }
             }
             e => {
                 panic!("Invalid options for Kafka input, use `{{\"group_id\": \"<group.id>\", \"topics\": [\"<topic>\"], \"brokers\": [\"<broker>\", \"<broker>\", ...]}}`\n{:?} ({})", e, opts)
@@ -87,38 +71,68 @@ impl Input {
 impl InputT for Input {
     fn enter_loop(&self, pipelines: Vec<mpsc::SyncSender<Msg>>) {
         let mut idx = 0;
-        for message in self.consumer.start().wait() {
-            idx += 1;
-            idx = idx % pipelines.len();
-            match message {
-                Err(_e) => {
-                    warn!("Input error");
-                }
-                Ok(Err(_m)) => {
-                    warn!("Input error");
-                }
-                Ok(Ok(m)) => {
-                    // Send a copy to the message to every output topic in parallel, and wait for the
-                    // delivery report to be received.
-                    if let Some(Ok(p)) = m.payload_view::<str>() {
-                        let key = match m.key_view::<str>() {
-                            Some(key) => Some(String::from(key.unwrap())),
-                            None => None,
-                        };
-                        if let Err(e) = pipelines[idx].send(Msg::new(key, String::from(p))) {
-                            error!("Error during handling message: {:?}", e)
-                        };
+        let mut t: Option<thread::JoinHandle<()>> = None;
+        for _tid in 0..self.config.threads {
+            let config = self.config.clone();
+            let pipelines = pipelines.clone();
+            let h = thread::spawn(move || {
+                let context = LoggingConsumerContext;
+                let consumer: LoggingConsumer = config.rdkafka_options.iter().fold(&mut ClientConfig::new(), |c: &mut ClientConfig, (k, v)| c.set(k, v))
+                        .set("group.id", &config.group_id)
+                        .set("bootstrap.servers", &config.brokers.join(","))
+                        .set("enable.partition.eof", "false")
+                        .set("session.timeout.ms", "6000")
+                    // Commit automatically every 5 seconds.
+                        .set("enable.auto.commit", "true")
+                        .set("auto.commit.interval.ms", "5000")
+                    // but only commit the offsets explicitly stored via `consumer.store_offset`.
+                        .set("enable.auto.offset.store", "false")
+                        .set_log_level(RDKafkaLogLevel::Debug)
+                        .create_with_context(context)
+                        .expect("Consumer creation failed");
+                let topics: Vec<&str> = config.topics.iter().map(|topic| topic.as_str()).collect();
+                consumer
+                    .subscribe(&topics)
+                    .expect("Can't subscribe to specified topic");
+                for message in consumer.start().wait() {
+                    idx += 1;
+                    idx = idx % pipelines.len();
+                    match message {
+                        Err(_e) => {
+                            warn!("Input error");
+                        }
+                        Ok(Err(_m)) => {
+                            warn!("Input error");
+                        }
+                        Ok(Ok(m)) => {
+                            // Send a copy to the message to every output topic in parallel, and wait for the
+                            // delivery report to be received.
+                            if let Some(Ok(p)) = m.payload_view::<str>() {
+                                let key = match m.key_view::<str>() {
+                                    Some(key) => Some(String::from(key.unwrap())),
+                                    None => None,
+                                };
+                                if let Err(e) = pipelines[idx].send(Msg::new(key, String::from(p)))
+                                {
+                                    error!("Error during handling message: {:?}", e)
+                                };
+                            }
+                            // Now that the message is completely processed, add it's position to the offset
+                            // store. The actual offset will be committed every 5 seconds.
+                            if let Err(e) = consumer.store_offset(&m) {
+                                INPUT_ERR.inc();
+                                warn!("Error while storing offset: {}", e);
+                            } else {
+                                INPUT_OK.inc();
+                            }
+                        }
                     }
-                    // Now that the message is completely processed, add it's position to the offset
-                    // store. The actual offset will be committed every 5 seconds.
-                    if let Err(e) = self.consumer.store_offset(&m) {
-                        INPUT_ERR.inc();
-                        warn!("Error while storing offset: {}", e);
-                    } else {
-                        INPUT_OK.inc();
-                    }
                 }
-            }
+            });
+            t = Some(h);
+        }
+        if let Some(h) = t {
+            let _ = h.join();
         }
     }
 }
