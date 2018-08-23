0e1cb186cc8f8beebdf1410b74918521069c2cb2
Heinz Gies <hgies@wayfair.com>
2018-08-23 13:01:01 +0200

    Use classifier in mimir

--- src/classifier/mimir.rs
+++ src/classifier/mimir.rs
@@ -1,88 +1,67 @@
 use classifier::Classification;
 use error::TSError;
-use mimir::{MimirError, RuleBuilder, RuleSet};
+use mimir;
 use pipeline::{Event, Step};
 use serde_json;
-use std::collections::HashMap;
 
 /// A constant classifier, it will classify all mesages the same way.
 #[derive(Debug)]
 pub struct Classifier {
-    // by keeping the RuleBuilder in scope we prevent it and by that
-    // the mimir reference to be freed.
-    builder: RuleBuilder,
-    rules: HashMap<usize, Classification>,
-    mimir_rules: RuleSet,
+    classifier: mimir::Classifier<Classification>,
 }
 
 impl Classifier {
     pub fn new(opts: &str) -> Self {
-        let mut builder = RuleBuilder::new();
-        let mut rules: HashMap<usize, Classification> = HashMap::new();
+        let mut classifier = mimir::Classifier::new();
         let classifications: Vec<Classification> =
             serde_json::from_str(opts).expect("Failed to parse classifications");
-        let mut i = 0;
         for c in &classifications {
             if let Some(ref rule) = c.rule {
-                rules.insert(i, c.clone());
-                if let Err(err) = builder.add_rule(rule) {
+                if let Err(err) = classifier.add(rule, c.clone()) {
                     panic!("Error parsing rule {:?} => {}", err, rule);
                 };
-                i += 1;
             }
         }
-        let mimir_rules = builder.done();
-        Classifier {
-            builder,
-            rules,
-            mimir_rules,
-        }
+        Classifier { classifier }
     }
 }
 
 impl Step for Classifier {
     fn apply(&mut self, event: Event) -> Result<Event, TSError> {
         // TODO: this clone is ugly
-        let mut doc = self.mimir_rules.document();
-        doc.load_json(event.raw.as_str())?;
-        let res = doc.first_match();
+        let doc = self.classifier.document_from_json(event.raw.as_str())?;
         let mut event = Event::from(event);
-        match res {
+        match self.classifier.classify(&doc) {
             None => {
                 event.classification = String::from("default");
             }
-            Some(rid) => match self.rules.get(&rid) {
-                Some(class) => {
-                    event.classification = class.class.clone();
-                    event.dimensions = class
-                        .clone()
-                        .dimensions
-                        .into_iter()
-                        .map(|key| match doc.find_string(&key) {
-                            Some(v) => v.clone(),
-                            None => String::from(""),
-                        })
-                        .collect();
-                    if let Some(ref key) = class.index_key {
-                        if let Some(v) = doc.find_string(key) {
-                            event.index = Some(v.clone());
-                        }
-                    }
-                    if let Some(v) = doc.find_string("type") {
-                        event.data_type = Some(v.clone());
+            Some(class) => {
+                event.classification = class.class.clone();
+                event.dimensions = class
+                    .clone()
+                    .dimensions
+                    .into_iter()
+                    .map(|key| match doc.find_string(&key) {
+                        Some(v) => v.clone(),
+                        None => String::from(""),
+                    })
+                    .collect();
+                if let Some(ref key) = class.index_key {
+                    if let Some(v) = doc.find_string(key) {
+                        event.index = Some(v.clone());
                     }
                 }
-                None => {
-                    event.classification = String::from("default");
+                if let Some(v) = doc.find_string("type") {
+                    event.data_type = Some(v.clone());
                 }
-            },
+            }
         };
         Ok(event)
     }
 }
 
-impl From<MimirError> for TSError {
-    fn from(from: MimirError) -> TSError {
+impl From<mimir::MimirError> for TSError {
+    fn from(from: mimir::MimirError) -> TSError {
         TSError::new(format!("{:?}", from).as_str())
     }
 }
