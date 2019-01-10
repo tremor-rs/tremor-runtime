0b84d74ab82702f9712bdfe11b006a59651110ef
Christian Burgas <cburgas@wayfair.com>
2019-01-10 05:21:43 -0500

    fixes return value of publish method

--- clients/php/src/Wayfair/Tremor/Client.php
+++ clients/php/src/Wayfair/Tremor/Client.php
@@ -1,11 +1,12 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Wayfair\Tremor;
 
-use GuzzleHttp;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException as GuzzleClientException;
+use GuzzleHttp\Stream\StreamInterface;
 use MessagePack\Packer;
-use Psr\Http\Message\StreamInterface;
 use Ramsey\Uuid\Uuid;
 use Wayfair\Tremor\Exceptions\ClientException;
 use Wayfair\Tremor\Exceptions\TremorException;
@@ -18,8 +19,7 @@ use Wayfair\Tremor\Proto;
  *
  * @package Wayfair\Tremor
  */
-class Client
-{
+class Client {
     /**
      * HTTP client isntance to talk to Tremor
      */
@@ -27,112 +27,91 @@ class Client
 
     /**
      * Hostname of the Tremor instnace
+     *
      * @var string
      */
     private $host;
 
     /**
      * Body encoding to be used
+     *
+     * @var string
      */
     private $body_encoding = 'json';
 
     /**
      * Event, or outer wrapper encoding to be used
+     *
+     * @var string
      */
     private $event_encoding = 'json';
 
     /**
      * Packer instance for msgpack encoding
+     *
      * @var Packer
      */
     private $packer;
 
     /**
      * Create a new Tremor Client Instance
-     * @param GuzzleHttp\Client $client client setup witht the URL of the tremor host to send events to.
-     * @param array $opts {
-     *      Optional. Options to be passed to control the behaviour of the client.
-     * @type string $body_encoding Encoding to use for the event body, 'json', and 'msgpack' are possible values.
-     * @type string $event_encoding Encoding for the event itself to use 'json', and 'protobuf' are valid values.
+     *
+     * @param GuzzleClient $client         client setup witht the URL of the tremor host to send events to.
+     * @param array        $opts           {
+     *                                     Optional. Options to be passed to control the behaviour of the client.
+     *
+     * @type string        $body_encoding  Encoding to use for the event body, 'json', and 'msgpack' are possible values.
+     * @type string        $event_encoding Encoding for the event itself to use 'json', and 'protobuf' are valid values.
      * }
      *
      * @throws ClientException if $body_encoding or $event_encoding are not supported values
      */
-    public function __construct(GuzzleHttp\Client $client, $opts = [])
-    {
+    public function __construct(GuzzleClient $client, $opts = []) {
         $this->client = $client;
-
-        $this->host = $client->getBaseUrl();
-        if ($this->host === '') {
-            throw new ClientException('Guzzle Client did not have a url');
-        }
+        $this->getHostFromClient($client);
 
         $this->setBodyEncoding($opts);
         $this->setEventEncoding($opts);
     }
 
     /**
-     * Sends an event to the Tremor host.
+     * @param GuzzleClient $client
      *
-     * @param array|string $body Data to be send to tremor, if the 'body-encoding' raw is used the body will be send as is without additional encoding.
-     * @param array $opts {
-     *      Optional. Options to be passed to control the behaviour of the.
-     * @type Uuid $parent_uuid If the event has a parent event, the parent UUID can be specified here.
-     * }
-     *
-     * @return StreamInterface Returns the phrase passed in
-     * @throws TremorException if there was an error when submitting the event.
+     * @throws ClientException
      */
-    public function publish($body, $opts = []): StreamInterface
-    {
-        try {
-            $header = new Proto\Event\Header([
-                'uuid' => Uuid::uuid1()->getBytes(),
-                'content_type' => Proto\Event\Header\ContentType::JSON
-            ]);
-        } catch (\Exception $exception) {
-            throw new TremorException("Header generation failed with the error '" . $exception->getMessage() . "'.");
-        }
-
-        if ($opts['parent_uuid'] !== null) {
-            $header->setParentUuid($opts['parent_uuid']->getBytes());
-        }
-
-        try {
-            $e = new Proto\Event(['header' => $header, 'body' => $this->getEncodedBody($body)]);
-            $renderedMsg = $this->getRenderedMessageFromEvent($e);
-            $contentType = $this->getContentTypeForRequest();
-
-            // Guzzle 6.0
-            // $response = $this->client->request('PUT', '/v1', ['ContentType' => $content_type, 'body' => $rendered_msg]);
-            $response = $this->client->put($this->host . '/v1',
-                ['headers' => ['Content-Type' => $contentType], 'body' => $renderedMsg]);
-
-            return $response->getBody();
-        } catch (GuzzleHttp\Exception\ClientException $e) {
-            $requestResponse = $e->getResponse();
-            if ($requestResponse === null) {
-                $exception = new TremorException('Client exception did not have a response embedded');
-                $exception->setHTTPError($e);
-
-                throw $exception;
-            }
-
-            $exception = new TremorException("Event submission failed with status code '" . $requestResponse->getStatusCode() . "'.");
-            $exception->setHTTPError($e);
-
-            throw $exception;
-        } catch (ClientException $e) {
-            throw new TremorException("Event submission failed with the error '" . $e->getMessage() . "'.");
+    private function getHostFromClient(GuzzleClient $client): void {
+        $this->host = $client->getBaseUrl();
+        if ($this->host === '') {
+            throw new ClientException('Guzzle Client did not have a url');
         }
     }
 
     /**
      * @param $opts
+     *
      * @throws ClientException
      */
-    protected function setEventEncoding($opts): void
-    {
+    private function setBodyEncoding($opts): void {
+        switch ($opts['body_encoding'] ?? null) {
+            case null:
+            case 'json':
+                $this->body_encoding = 'json';
+                break;
+            case 'msgpack':
+                $this->packer        = new Packer();
+                $this->body_encoding = 'msgpack';
+                break;
+            default:
+                throw new ClientException("The body_encoding '" . $opts['body_encoding'] . "' is not supported");
+        }
+    }
+
+    /**
+     * @param $opts
+     *
+     * @throws ClientException
+     */
+    private function setEventEncoding($opts): void {
         switch ($opts['event_encoding'] ?? null) {
             case null:
             case 'json':
@@ -147,32 +126,113 @@ class Client
     }
 
     /**
-     * @param $opts
-     * @throws ClientException
+     * Sends an event to the Tremor host.
+     *
+     * @param array|string $body        Data to be send to tremor, if the 'body-encoding' raw is used the body will be send as is without additional encoding.
+     * @param array        $opts        {
+     *                                  Optional. Options to be passed to control the behaviour of the.
+     *                                   @type Uuid $parent_uuid If the event has a parent event, the parent UUID can be specified here.
+     *                                  }
+     *
+     * @return StreamInterface Returns the phrase passed in
+     * @throws TremorException if there was an error when submitting the event.
      */
-    protected function setBodyEncoding($opts): void
-    {
-        switch ($opts['body_encoding'] ?? null) {
-            case null:
-            case 'json':
-                $this->body_encoding = 'json';
-                break;
-            case 'msgpack':
-                $this->packer = new Packer();
-                $this->body_encoding = 'msgpack';
-                break;
-            default:
-                throw new ClientException("The body_encoding '" . $opts['body_encoding'] . "' is not supported");
+    public function publish($body, $opts = []): StreamInterface {
+        $header = $this->createHeader($opts);
+
+        try {
+            return $this->publishBodyWithHeaderToTremor($body, $header);
+        } catch (GuzzleClientException $e) {
+            $this->throwExceptionFromGuzzleClientException($e);
+        }
+
+        throw new TremorException('could not publish the message');
+    }
+
+    /**
+     * @param array $opts
+     *
+     * @return Proto\Event\Header
+     * @throws TremorException
+     */
+    private function createHeader(array $opts): Proto\Event\Header {
+        try {
+            $headerData = [
+                'uuid'         => Uuid::uuid1()->getBytes(),
+                'content_type' => Proto\Event\Header\ContentType::JSON,
+            ];
+            $header     = new Proto\Event\Header($headerData);
+
+            if ($opts['parent_uuid'] ?? false) {
+                $header->setParentUuid($opts['parent_uuid']->getBytes());
+            }
+        } catch (\Exception $exception) {
+            throw new TremorException("Header generation failed with the error '" . $exception->getMessage() . "'.");
+        }
+
+        return $header;
+    }
+
+    /**
+     * @param                    $body
+     * @param Proto\Event\Header $header
+     *
+     * @return StreamInterface|null
+     * @throws TremorException
+     */
+    private function publishBodyWithHeaderToTremor($body, Proto\Event\Header $header): StreamInterface {
+        $uri      = $this->host . '/v1';
+        $options  = $this->createOptionsForBodyAndHeader($body, $header);
+        $response = $this->client->put($uri, $options);
+        if ($response === null) {
+            throw new TremorException('did not get a response from request');
+        }
+
+        $responseBody = $response->getBody();
+        if ($responseBody === null) {
+            throw new TremorException('response did not have a body');
+        }
+
+        return $responseBody;
+    }
+
+    /**
+     * @param                    $body
+     * @param Proto\Event\Header $header
+     *
+     * @return array
+     * @throws TremorException
+     */
+    private function createOptionsForBodyAndHeader($body, Proto\Event\Header $header): array {
+        try {
+            $e           = $this->createEventForBodyAndHeader($body, $header);
+            $renderedMsg = $this->getRenderedMessageFromEvent($e);
+            $contentType = $this->getContentTypeForRequest();
+
+            return ['headers' => ['Content-Type' => $contentType], 'body' => $renderedMsg];
+        } catch (ClientException $e) {
+            throw new TremorException("Event submission failed with the error '" . $e->getMessage() . "'.");
         }
     }
 
     /**
+     * @param                    $body
+     * @param Proto\Event\Header $header
+     *
+     * @return Proto\Event
+     * @throws ClientException
+     */
+    private function createEventForBodyAndHeader($body, Proto\Event\Header $header): Proto\Event {
+        return new Proto\Event(['header' => $header, 'body' => $this->getEncodedBody($body)]);
+    }
+
+    /**
      * @param mixed $body the body to encode
+     *
      * @return string
      * @throws ClientException
      */
-    protected function getEncodedBody($body): string
-    {
+    private function getEncodedBody($body): string {
         switch ($this->body_encoding) {
             case 'json':
                 return json_encode($body);
@@ -191,8 +251,7 @@ class Client
      * @return string
      * @throws ClientException
      */
-    protected function getRenderedMessageFromEvent(Proto\Event $e): string
-    {
+    private function getRenderedMessageFromEvent(Proto\Event $e): string {
         switch ($this->event_encoding) {
             case 'json':
                 return $e->serializeToJsonString();
@@ -211,8 +270,7 @@ class Client
      * @return string
      * @throws ClientException
      */
-    protected function getContentTypeForRequest(): string
-    {
+    private function getContentTypeForRequest(): string {
         switch ($this->event_encoding) {
             case 'json':
                 return 'application/json+tremor.v1';
@@ -223,5 +281,26 @@ class Client
         }
 
         throw new ClientException('The event_encoding is not supported');
+    }
+
+    /**
+     * @param GuzzleClientException $e
+     *
+     * @throws TremorException
+     */
+    private function throwExceptionFromGuzzleClientException(GuzzleClientException $e): void {
+        $requestResponse = $e->getResponse();
+        if ($requestResponse === null) {
+            $exception = new TremorException('Client exception did not have a response embedded');
+            $exception->setHTTPError($e);
+
+            throw $exception;
+        }
+
+        $exception =
+            new TremorException("Event submission failed with status code '" . $requestResponse->getStatusCode() . "'.");
+        $exception->setHTTPError($e);
+
+        throw $exception;
     }
 }
