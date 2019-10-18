d028af12ba09de06c8edc9fec96dbdb36a2a6626
Heinz Gies <hgies@wayfair.com>
2019-10-18 07:26:54 +0200

    Update CI workflow

---
+++ checks/copyright.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+count=0
+
+for file in $(find . -name '*.rs' | grep -v '/target')
+do
+    if ! grep 'Copyright 2018-2019, Wayfair GmbH' "$file" > /dev/null
+    then
+        echo "##[error] Copyright missing in $file"
+        count=$((count + 1))
+    fi
+done
+
+exit $count
