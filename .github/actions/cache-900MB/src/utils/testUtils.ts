d4f771bfe90f5104b8dc73d99cae149fcd2ec42b
Heinz Gies <hgies@wayfair.com>
2020-01-07 11:14:51 +0100

    Run tarpaulin on stable and remove cache clone

--- .github/actions/cache-900MB/src/utils/testUtils.ts
+++
@@ -1,7 +0,0 @@
-function getInputName(name: string): string {
-    return `INPUT_${name.replace(/ /g, "_").toUpperCase()}`;
-}
-
-export function setInput(name: string, value: string) {
-    process.env[getInputName(name)] = value;
-}
