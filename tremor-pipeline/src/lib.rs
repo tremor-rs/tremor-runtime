63b82b010e36bdf08143e7661e6c993659916de0
Heinz Gies <hgies@wayfair.com>
2020-01-07 11:11:14 +0100

    Add must use allow to window

--- tremor-pipeline/src/lib.rs
+++ tremor-pipeline/src/lib.rs
@@ -14,17 +14,14 @@
 
 #![forbid(warnings)]
 #![recursion_limit = "1024"]
-#![cfg_attr(
-    feature = "cargo-clippy",
-    deny(
-        clippy::all,
-        clippy::result_unwrap_used,
-        clippy::option_unwrap_used,
-        clippy::unnecessary_unwrap,
-        clippy::pedantic
-    ),
-    allow(clippy::must_use_candidate)
+#![deny(
+    clippy::all,
+    clippy::result_unwrap_used,
+    clippy::option_unwrap_used,
+    clippy::unnecessary_unwrap,
+    clippy::pedantic
 )]
+#![allow(clippy::must_use_candidate)]
 
 #[macro_use]
 extern crate serde_derive;
