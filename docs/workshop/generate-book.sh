e0c588a655178b7cd995003540759501043baa15
Heinz Gies <hgies@wayfair.com>
2020-01-20 15:48:09 +0100

    Add passthrough workshop example

--- docs/workshop/generate-book.sh
+++ docs/workshop/generate-book.sh
@@ -7,6 +7,10 @@ if [ ! -d src ]; then
 fi
 
 echo "[Introduction](introduction.md)\n" > src/SUMMARY.md
+echo "  - [1. Passthrough](examples/passthrough/README.md)]" > src/SUMMARY.md
+echo "  - [1. Filter](examples/filter/README.md)]" > src/SUMMARY.md
+echo "  - [1. Transform](examples/transform/README.md)]" > src/SUMMARY.md
+echo "  - [1. Validate](examples/validate/README.md)]" > src/SUMMARY.md
 
 for f in $(ls text/* | sort)
 do
---
+++ docs/workshop/src/SUMMARY.md
@@ -0,0 +1,2 @@
+[Introduction](introduction.md)
+
