f825b5e8910ab5b986d7cf44539df20dc8fd09a9
Darach Ennis <dennis@wayfair.com>
2020-02-10 15:42:26 +0100

    Cleanup benchmarks

--- bench/all.sh
+++ bench/all.sh
@@ -4,6 +4,6 @@
 ./bench/run empty-pipeline-msgpack
 ./bench/run empty-pipeline-two-inputs
 ./bench/run empty-pipeline
-./bench/run real-workflow-througput-json
-# ./bench/run real-workflow-througput-msgpack
+./bench/run real-workflow-throughput-json
+# ./bench/run real-workflow-throughput-msgpack
 ./bench/run real-workflow
