1db69dcc20fb1149f39a3bca340ab0d7714ba2fc
Ernad Halilović <ernadh+github@gmail.com>
2020-02-20 17:34:49 +0100

    Postgres onramp, offramp; mmap based kv (#56)
    * Postgres onramp, offramp; mmap based kv
    
    * Return in cache
    
    * Disable cache in integration tests
    
    * Remove unnecessary usage of unsafe
    
    * Add Build step back

---
+++ src/offramp/postgres.rs
@@ -0,0 +1,152 @@
+// Copyright 2018-2020, Wayfair GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! # `PostgreSQL` Offramp
+//!
+//! Writes events to a `PostgreSQL` and `TimescaleDB` database
+//!
+//! ## Configuration
+//!
+//! See [Config](struct.Config.html) for details.
+
+use crate::offramp::prelude::*;
+use crate::ramp::postgres::{json_to_record, Record};
+use halfbrown::HashMap;
+use postgres::{Client, NoTls};
+use simd_json::value::Value;
+
+pub struct Postgres {
+    pub config: Config,
+    client: Option<postgres::Client>,
+    pipelines: HashMap<TremorURL, PipelineAddr>,
+    postprocessors: Postprocessors,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Config {
+    pub host: String,
+    pub port: u32,
+    pub user: String,
+    pub password: String,
+    pub dbname: String,
+    pub table: String,
+}
+
+impl ConfigImpl for Config {}
+
+impl offramp::Impl for Postgres {
+    fn from_config(config: &Option<OpConfig>) -> Result<Box<dyn Offramp>> {
+        if let Some(config) = config {
+            let config: Config = Config::new(config)?;
+
+            Ok(Box::new(Self {
+                config,
+                pipelines: HashMap::new(),
+                postprocessors: vec![],
+                client: None,
+            }))
+        } else {
+            Err("Missing config for onramp".into())
+        }
+    }
+}
+
+fn init_cli(config: &Config) -> std::result::Result<postgres::Client, postgres::Error> {
+    let conn_str = format!(
+        "host={} user={} password={} port={} dbname={}",
+        config.host, config.user, config.password, config.port, config.dbname
+    );
+    let cli = Client::connect(&conn_str, NoTls)?;
+
+    Ok(cli)
+}
+
+impl Offramp for Postgres {
+    fn on_event(&mut self, _codec: &Box<dyn Codec>, _input: String, event: Event) -> Result<()> {
+        for val in event.value_iter() {
+            let obj = val.as_object();
+            if let Some(kv) = obj {
+                let mut fields: Vec<String> = Vec::new();
+                let mut params: Vec<String> = Vec::new();
+                let mut records: Vec<Record> = Vec::new();
+
+                let mut ct: usize = 1;
+                for (field, value) in kv {
+                    fields.push(field.to_string());
+                    params.push(format!("${}", ct));
+                    ct += 1;
+                    let record = match json_to_record(value) {
+                        Ok(v) => v,
+                        Err(e) => {
+                            return Err(format!("Could not convert json to record: {}", e).into())
+                        }
+                    };
+                    records.push(record);
+                }
+
+                let fields = fields.join(",");
+                let params = params.join(",");
+
+                let q = format!(
+                    "INSERT INTO {} ({}) VALUES ({});",
+                    self.config.table, fields, params
+                );
+                let s_slice: &str = &q[..];
+
+                if self.client.is_none() {
+                    self.client = match init_cli(&self.config) {
+                        Ok(v) => Some(v),
+                        Err(e) => {
+                            return Err(
+                                format!("Could not initialize a Postgres client: {}", e).into()
+                            )
+                        }
+                    }
+                }
+
+                let client = match self.client.as_mut() {
+                    Some(v) => v,
+                    None => return Err("could not move client value".into()),
+                };
+
+                match client.query_raw(
+                    s_slice,
+                    records
+                        .iter()
+                        .map(|p| p as &dyn postgres::types::ToSql)
+                        .collect::<Vec<&dyn postgres::types::ToSql>>(),
+                ) {
+                    Ok(_) => return Ok(()),
+                    Err(e) => return Err(format!("Failure while querying: {}", e).into()),
+                }
+            }
+        }
+
+        Ok(())
+    }
+    fn add_pipeline(&mut self, id: TremorURL, addr: PipelineAddr) {
+        self.pipelines.insert(id, addr);
+    }
+    fn remove_pipeline(&mut self, id: TremorURL) -> bool {
+        self.pipelines.remove(&id);
+        self.pipelines.is_empty()
+    }
+    fn default_codec(&self) -> &str {
+        "json"
+    }
+    fn start(&mut self, _codec: &Box<dyn Codec>, postprocessors: &[String]) -> Result<()> {
+        self.postprocessors = make_postprocessors(postprocessors)?;
+        Ok(())
+    }
+}
---
+++ src/ramp.rs
@@ -0,0 +1,259 @@
+// Copyright 2018-2020, Wayfair GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use crate::errors::*;
+use crate::utils::ConfigImpl;
+use memmap::MmapOptions;
+use std::cmp;
+use std::fs::OpenOptions;
+use std::io;
+use std::io::Write;
+use std::ops::DerefMut;
+use std::path::Path;
+
+pub mod postgres;
+
+pub trait KV {
+    fn get(&mut self) -> Result<simd_json::OwnedValue>;
+    fn set(&mut self, obj: simd_json::OwnedValue) -> Result<()>;
+}
+
+pub struct MmapFile {
+    pub config: Config,
+    pub store: memmap::MmapMut,
+    pub len: usize,
+    pub end: usize,
+}
+pub struct MmapAnon {
+    pub store: memmap::MmapMut,
+    pub end: usize,
+    pub len: usize,
+}
+
+impl MmapFile {
+    fn as_mut_slice(&mut self) -> &mut [u8] {
+        &mut self.store[..self.end]
+    }
+}
+
+impl MmapAnon {
+    fn as_slice(&self) -> &[u8] {
+        &self.store[..self.end]
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [u8] {
+        &mut self.store[..self.end]
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.store.flush()
+    }
+}
+
+impl KV for MmapFile {
+    fn get(&mut self) -> Result<simd_json::OwnedValue> {
+        let obj = simd_json::to_owned_value(self.as_mut_slice())?;
+
+        Ok(obj)
+    }
+
+    fn set(&mut self, obj: simd_json::OwnedValue) -> Result<()> {
+        let string = obj.encode();
+        let bytes = string.as_bytes();
+        if bytes.len() > self.len {
+            return Err("object too large to store in memory-mapped file".into());
+        }
+        self.store.deref_mut().write_all(bytes)?;
+        self.end = bytes.len();
+
+        Ok(())
+    }
+}
+
+impl KV for MmapAnon {
+    fn get(&mut self) -> Result<simd_json::OwnedValue> {
+        let mmap = self.as_slice();
+        let mut bytes: Vec<u8> = Vec::with_capacity(mmap.len());
+        bytes.extend_from_slice(mmap);
+        let obj = simd_json::to_owned_value(&mut bytes)?;
+
+        Ok(obj)
+    }
+
+    fn set(&mut self, obj: simd_json::OwnedValue) -> Result<()> {
+        let string = obj.encode();
+        let bytes = string.as_bytes();
+        if self.store.len() < bytes.len() {
+            let new_size = bytes.len();
+            let len = cmp::max(self.len + self.len, new_size);
+            let new_store = MmapOptions::new().len(len).map_anon()?;
+            let new_mmap = Self {
+                store: new_store,
+                len,
+                end: 0,
+            };
+            *self = new_mmap;
+        }
+        self.end = bytes.len();
+        self.as_mut_slice().copy_from_slice(bytes);
+        self.flush()?;
+
+        Ok(())
+    }
+}
+
+impl ConfigImpl for Config {}
+
+// FIXME: Same config for both types of mmap as this stands
+#[derive(Deserialize, Debug, Clone)]
+pub struct Config {
+    pub path: String,
+    pub size: usize,
+}
+
+impl MmapAnon {
+    fn from_config(config: Option<Config>, obj: &simd_json::OwnedValue) -> Result<Box<dyn KV>> {
+        if let Some(_config) = config {
+            let string = obj.encode();
+            let bytes = string.as_bytes();
+            let len = bytes.len();
+            let mut store = MmapOptions::new().len(len).map_anon()?;
+            store.copy_from_slice(bytes);
+            store.flush()?;
+
+            Ok(Box::new(Self {
+                store,
+                end: len,
+                len,
+            }))
+        } else {
+            Err("Missing config for mmap".into())
+        }
+    }
+}
+
+impl MmapFile {
+    fn from_config(config: Option<Config>, obj: &simd_json::OwnedValue) -> Result<Box<dyn KV>> {
+        if let Some(config) = config {
+            let p = Path::new(&config.path);
+            let mut file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .create(true)
+                .open(p)?;
+            file.set_len(config.size as u64)?;
+            let len = config.size as usize;
+            let string = obj.encode();
+            let bytes = string.as_bytes();
+            let end = bytes.len();
+            file.write_all(&bytes)?;
+            let store = unsafe { MmapOptions::new().map(&file)? };
+            let store = store.make_mut()?;
+
+            Ok(Box::new(Self {
+                config,
+                store,
+                len,
+                end,
+            }))
+        } else {
+            Err("Missing config for mmap".into())
+        }
+    }
+}
+
+#[cfg_attr(tarpaulin, skip)]
+pub fn lookup(
+    name: &str,
+    config: Option<Config>,
+    obj: &simd_json::OwnedValue,
+) -> Result<Box<dyn KV>> {
+    match name {
+        "mmap_file" => MmapFile::from_config(config, &obj),
+        "mmap_anon" => MmapAnon::from_config(config, &obj),
+        _ => Err(format!("Cache {} not known", name).into()),
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+
+    use super::{Config, MmapAnon, MmapFile};
+    use std::fs::File;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_mmap_anon() {
+        let config = Config {
+            path: "/this/is/never/used.json".to_string(),
+            size: 4096,
+        };
+        let mut data = "{\"foo\": \"bar\"}".to_string();
+        let bytes = unsafe { data.as_bytes_mut() };
+        let opt = std::option::Option::from(config);
+        let obj = simd_json::to_owned_value(bytes).unwrap();
+        let exp_obj = obj.clone();
+        let mut mmap = MmapAnon::from_config(opt, &obj).expect("To create anon memory map");
+
+        assert_eq!(mmap.get().expect("To retrieve object"), exp_obj);
+
+        let mut data2 = "{\"snot\": \"badger\"}".to_string();
+        let bytes2 = unsafe { data2.as_bytes_mut() };
+        let obj2 = simd_json::to_owned_value(bytes2).unwrap();
+        let exp_obj2 = obj2.clone();
+        mmap.set(obj2).expect("To set object in mmap");
+        let ret_obj = mmap.get().expect("To retrieve object");
+
+        assert_eq!(ret_obj, exp_obj2);
+    }
+
+    #[test]
+    fn test_mmap_file() {
+        let dir = tempdir().unwrap();
+        let file_path = dir.path().join("storage.json");
+        let file = File::create(file_path.clone()).unwrap();
+        let config = Config {
+            path: file_path.as_path().to_string_lossy().to_string(),
+            size: 12,
+        };
+        let opt = std::option::Option::from(config);
+        let mut data = "[0,1,2,3,4]".to_string();
+        let bytes = unsafe { data.as_bytes_mut() };
+        let obj = simd_json::to_owned_value(bytes).unwrap();
+        let exp_obj = obj.clone();
+        let mut mmap = MmapFile::from_config(opt, &obj).expect("To create file-backed memory map");
+
+        assert_eq!(mmap.get().expect("To retrieve object"), exp_obj);
+
+        let mut data2 = "[5,6,7,8,9]".to_string();
+        let bytes2 = unsafe { data2.as_bytes_mut() };
+        let obj2 = simd_json::to_owned_value(bytes2).unwrap();
+        let exp_obj2 = obj2.clone();
+        mmap.set(obj2).expect("To set object in mmap");
+
+        assert_eq!(mmap.get().expect("To retrieve object"), exp_obj2);
+
+        let mut data3 = "{\"foozah\": \"barah\"}".to_string();
+        let bytes3 = unsafe { data3.as_bytes_mut() };
+        let obj3 = simd_json::to_owned_value(bytes3).unwrap();
+
+        assert!(
+            mmap.set(obj3).is_err(),
+            "object too large to store in memory-mapped file"
+        );
+
+        drop(file);
+        dir.close().unwrap();
+    }
+}
