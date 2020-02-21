24b46990d05f92d503be3b61474046add5471233
Heinz N. Gies <heinz@licenser.net>
2020-02-21 12:19:59 +0100

    Update postgres for no actix changes

--- src/onramp/ws.rs
+++ src/onramp/ws.rs
@@ -21,7 +21,7 @@ use crossbeam_channel::{select, Sender};
 use serde_yaml::Value;
 use std::io;
 use std::thread;
-
+/*
 type ActixResult<T> = std::result::Result<T, ActixError>;
 
 #[derive(Deserialize, Debug, Clone)]
@@ -255,6 +255,82 @@ fn onramp_loop(
         }
     }
 }
+*/
+use async_std::net::{TcpListener, TcpStream};
+use async_std::task;
+use tungstenite::protocol::Message;
+
+async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
+    println!("Incoming TCP connection from: {}", addr);
+
+    let ws_stream = async_tungstenite::accept_async(raw_stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+    println!("WebSocket connection established: {}", addr);
+
+    // Insert the write part of this peer to the peer map.
+    let (tx, rx) = unbounded();
+    peer_map.lock().unwrap().insert(addr, tx);
+
+    let (outgoing, incoming) = ws_stream.split();
+
+    let broadcast_incoming = incoming.try_for_each(|msg| {
+        println!(
+            "Received a message from {}: {}",
+            addr,
+            msg.to_text().unwrap()
+        );
+        let peers = peer_map.lock().unwrap();
+
+        // We want to broadcast the message to everyone except ourselves.
+        let broadcast_recipients = peers
+            .iter()
+            .filter(|(peer_addr, _)| peer_addr != &&addr)
+            .map(|(_, ws_sink)| ws_sink);
+
+        for recp in broadcast_recipients {
+            recp.unbounded_send(msg.clone()).unwrap();
+        }
+
+        future::ok(())
+    });
+
+    let receive_from_others = rx.map(Ok).forward(outgoing);
+
+    pin_mut!(broadcast_incoming, receive_from_others);
+    future::select(broadcast_incoming, receive_from_others).await;
+
+    println!("{} disconnected", &addr);
+    peer_map.lock().unwrap().remove(&addr);
+}
+
+
+async fn onramp_loop(
+    rx: &Receiver<onramp::Msg>,
+    config: Config,
+    preprocessors: Vec<String>,
+    mut codec: Box<dyn Codec>,
+    mut metrics_reporter: RampReporter
+ ) -> Result<(), IoError> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
+
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await;
+    let listener = try_socket.expect("Failed to bind");
+    println!("Listening on: {}", addr);
+
+    // Let's spawn the handling of each connection in a separate task.
+    while let Ok((stream, addr)) = listener.accept().await {
+        task::spawn(handle_connection(state.clone(), stream, addr));
+    }
+
+    Ok(())
+}
+
 
 impl Onramp for Ws {
     fn start(
@@ -268,10 +344,10 @@ impl Onramp for Ws {
         let codec = codec::lookup(&codec)?;
         // we need to change this here since ws is special
         let preprocessors = preprocessors.to_vec();
-        thread::Builder::new()
+        task::Builder::new()
             .name(format!("onramp-ws-{}", "???"))
             .spawn(move || {
-                if let Err(e) = onramp_loop(&rx, config, preprocessors, codec, metrics_reporter) {
+                if let Err(e) = onramp_loop(&rx, config, preprocessors, codec, metrics_reporter).await {
                     error!("[Onramp] Error: {}", e)
                 }
             })?;
