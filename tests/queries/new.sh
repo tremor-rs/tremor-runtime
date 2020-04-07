8742ae57a997f79523a7403208f8306a750d60df
Heinz N. Gies <heinz@licenser.net>
2020-04-07 10:36:17 +0200

    Add new.sh for queries

---
+++ tests/queries/new.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+BASEDIR=$(dirname "$0")
+if [ -z "x${1}x" ]
+then
+	echo "Please give the test a name";
+fi
+
+NAME="${1}"
+TARGET="${BASEDIR}/${1}"
+
+if [ -d "${TARGET}" ]
+then
+	echo "Please give the test a name";
+fi
+
+cp -r ${BASEDIR}/_template ${TARGET}
+
+sed -e "s;//INSERT;//INSERT\n    ${NAME},;" ${BASEDIR}/../query.rs > ${BASEDIR}/tmp && mv ${BASEDIR}/tmp ${BASEDIR}/../query.rs
