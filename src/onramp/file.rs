a2ea60833f172d12971982ec65889c0c5baa759c
Heinz N. Gies <heinz@licenser.net>
2020-05-18 12:26:57 +0200

    Try moving the sleep

--- src/onramp/file.rs
+++ src/onramp/file.rs
@@ -109,8 +109,8 @@ fn onramp_loop(
     }
 
     //TODO: This is super gugly:
+    thread::sleep(Duration::from_millis(500));
     if config.close_on_done {
-        thread::sleep(Duration::from_millis(500));
         // ALLOW: This is on purpose, close when done tells the onramp to terminate when it's done with sending it's data - this is for one off's
         process::exit(0);
     }
