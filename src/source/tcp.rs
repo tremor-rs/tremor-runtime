f770c6b2dcc22876e147b694d73ebf7e9cc11755
Heinz N. Gies <heinz@licenser.net>
2020-08-06 18:16:46 +0200

    Move UDP from onramp/mio to source/async_std
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- src/source/tcp.rs
+++ src/source/tcp.rs
@@ -55,7 +55,7 @@ pub struct Int {
 }
 impl std::fmt::Debug for Int {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Tcp")
+        write!(f, "TCP")
     }
 }
 impl Int {
