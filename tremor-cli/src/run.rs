b7f106b32194c157f7b7fcbf6c7db0aa2c7c94e8
Heinz N. Gies <heinz@licenser.net>
2020-09-10 17:16:13 +0200

    reinstalte heredoc tests

--- tremor-cli/src/run.rs
+++ tremor-cli/src/run.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use crate::errors::{Error, Result};
 use crate::util::*;
 use clap::ArgMatches;
