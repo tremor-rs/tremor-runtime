1e379c0eafb44ce0b57a180c334d94edfe872650
Anup Dhamala <anupdhml+git@gmail.com>
2020-09-14 16:31:15 -0400

    Fix script to publish builder images
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- packaging/builder-images/publish_images.sh
+++ packaging/builder-images/publish_images.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-# 
+#
 # publish_images.sh
 #
 # Publish all the project builder docker images to docker hub
 #
+# TODO integrate logic here with project Makefile
+#
 # Usage: ./publish_images.sh
+
 # exit the script when a command fails
 set -o errexit
+
 # catch exit status for piped commands
 set -o pipefail
-RUST_TOOLCHAIN_FILE="./rust-toolchain"
+
+RUST_TOOLCHAIN_FILE="../../rust-toolchain"
 RUST_VERSION=$(<"$RUST_TOOLCHAIN_FILE")
+
 IMAGES=(
   "tremor-builder:x86_64-unknown-linux-gnu"
   "tremor-builder:x86_64-unknown-linux-gnu-${RUST_VERSION}"
@@ -20,5 +26,5 @@ IMAGES=(
   #"tremor-builder:x86_64-unknown-linux-musl-${RUST_VERSION}"
 )
 for image in "${IMAGES[@]}"; do
-  docker push tremorproject/"$image"
+  docker push "tremorproject/${image}"
 done
