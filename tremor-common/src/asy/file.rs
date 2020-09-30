af81aab2816740eb8cdabcbc70454ba3f50339b1
Heinz N. Gies <heinz@licenser.net>
2020-09-30 13:05:15 +0200

    Obey clippy
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tremor-common/src/asy/file.rs
+++ tremor-common/src/asy/file.rs
@@ -1,7 +1,24 @@
+// Copyright 2020, The Tremor Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 use async_std::{fs::File, path::Path};
 
 use crate::errors::Error;
 
+/// A wrapper around `File::open` that will give a better error (including the filename)
+///
+/// # Errors
+///   * if the file couldn't be opened
 pub async fn open<S>(path: &S) -> Result<File, Error>
 where
     S: AsRef<Path> + ?Sized,
--- tremor-common/src/lib.rs
+++ tremor-common/src/lib.rs
@@ -1,6 +1,33 @@
+// Copyright 2020, The Tremor Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//! Tremmor common utility functions
+#![forbid(warnings)]
+#![deny(missing_docs)]
+#![recursion_limit = "1024"]
+#![deny(
+    clippy::all,
+    clippy::unwrap_used,
+    clippy::unnecessary_unwrap,
+    clippy::pedantic
+)]
+
+/// functions for async related code
 pub mod asy;
-pub mod errors;
+mod errors;
+/// File related functions
 pub mod file;
+/// Time related functions
 pub mod time;
 
 pub use errors::Error;
