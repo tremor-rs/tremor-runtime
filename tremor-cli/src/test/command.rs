e30d2c13c7c9b97bc07f2dd60243e48cb4f770d7
Heinz N. Gies <heinz@licenser.net>
2020-09-30 10:12:51 +0200

    Obey clippy
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tremor-cli/src/test/command.rs
+++ tremor-cli/src/test/command.rs
@@ -116,10 +116,11 @@ pub(crate) fn suite_command(
                     let args = shell_words::split(&case.command).unwrap_or_default();
 
                     if let Some((cmd, args)) = args.split_first() {
-                        let cmd = job::which(&cmd)?;
+                        let resolved_cmd = job::which(&cmd)?;
 
                         // FIXME wintel
-                        let mut fg_process = job::TargetProcess::new_with_stderr(&cmd, &args)?;
+                        let mut fg_process =
+                            job::TargetProcess::new_with_stderr(&resolved_cmd, &args)?;
                         let exit_status = fg_process.wait_with_output();
 
                         let fg_out_file = format!("{}/fg.{}.out.log", base.clone(), counter);
