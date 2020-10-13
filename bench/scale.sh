d314e05a46ad84050b6d5de6e861f67935183f2f
Heinz N. Gies <heinz@licenser.net>
2020-10-13 13:45:43 +0200

    Fix scale bench
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- bench/scale.sh
+++ bench/scale.sh
@@ -1,4 +1,3 @@
-
 if [ -f "$1" ]
 then
 	TARGET=$1
@@ -9,11 +8,11 @@ fi
 echo "(3 cores)"
 for i in 1 2 4 8 16 32 64 
 do
-	taskset -c 0,1,2 $TARGET server run --no-api -f ./bench/real-workflow-throughput-json-$i-$i-1.yaml bench/link.yaml 2>&1 | grep Throughput | sed -e 's/Throughput: //' -e 's; MB/s;;'
+	taskset -c 0,1,2 $TARGET server run --no-api -f ./bench/real-workflow-throughput-json-$i-$i-1.yaml ./bench/real-workflow-throughput-json-$i-$i-1/* bench/link.yaml 2>&1 | grep Throughput | sed -e 's/Throughput: //' -e 's; MB/s;;'
 done
 
 echo "(all cores)"
 for i in 1 2 4 8 16 32 64
 do
-	$TARGET server run --no-api -f ./bench/real-workflow-throughput-json-$i-$i-1.yaml bench/link.yaml 2>&1 | grep Throughput | sed -e 's/Throughput: //' -e 's; MB/s;;'
+	$TARGET server run --no-api -f ./bench/real-workflow-throughput-json-$i-$i-1.yaml ./bench/real-workflow-throughput-json-$i-$i-1/* ./bench/link.yaml 2>&1 | grep Throughput | sed -e 's/Throughput: //' -e 's; MB/s;;'
 done
