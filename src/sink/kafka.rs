eec1a886785b0f7e4a7905595b58ea22873488ad
Matthias Wahl <mwahl@wayfair.com>
2020-10-22 10:58:40 +0200

    Update to rdkafka git tag to avoid explicit dependency on librdkafka-sys.
    Signed-off-by: Matthias Wahl <mwahl@wayfair.com>

--- src/sink/kafka.rs
+++ src/sink/kafka.rs
@@ -125,24 +125,6 @@ fn is_fatal(e: &KafkaError) -> bool {
     }
 }
 
-unsafe fn get_fatal_error<C>(
-    client: &rdkafka::client::Client<C>,
-) -> Option<(rdkafka::types::RDKafkaRespErr, String)>
-where
-    C: rdkafka::ClientContext,
-{
-    const LEN: usize = 4096;
-    let mut buf: [i8; LEN] = std::mem::MaybeUninit::uninit().assume_init();
-    let client_ptr = client.native_ptr();
-
-    let code = rdkafka_sys::bindings::rd_kafka_fatal_error(client_ptr, buf.as_mut_ptr(), LEN);
-    if code == rdkafka::types::RDKafkaRespErr::RD_KAFKA_RESP_ERR_NO_ERROR {
-        None
-    } else {
-        Some((code, rdkafka::util::cstr_to_owned(buf.as_ptr())))
-    }
-}
-
 #[async_trait::async_trait]
 impl Sink for Kafka {
     async fn on_event(
@@ -174,9 +156,7 @@ impl Sink for Kafka {
                 Err((e, _r)) => {
                     error!("[Kafka Offramp] failed to enque message: {}", e);
                     if is_fatal(&e) {
-                        if let Some((code, fatal)) =
-                            unsafe { get_fatal_error(self.producer.client()) }
-                        {
+                        if let Some((code, fatal)) = self.producer.client().fatal_error() {
                             error!("[Kafka Offramp] Fatal Error({:?}): {}", code, fatal);
                         }
                         self.producer = self.config.producer()?;
