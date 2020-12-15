cde89841b8c5e6f17c72b3f1592dde77986fe0a9
Heinz N. Gies <heinz@licenser.net>
2020-12-15 14:35:47 +0100

    Fix errros from value
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tremor-value/src/error.rs
+++ tremor-value/src/error.rs
@@ -30,7 +30,11 @@ pub enum Error {
 
 impl Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:?}", self)
+        match self {
+            Error::ExpectedMap => write!(f, "Expected a struct, but did not find out"),
+            Error::Serde(s) => f.write_str(&s),
+            Error::SimdJSON(e) => write!(f, "SIMD JSON error: {}", e),
+        }
     }
 }
 
