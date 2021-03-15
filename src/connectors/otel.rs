46386811514bf03f6c7252e022e0032fc02b946e
Darach Ennis <darach@gmail.com>
2021-03-15 19:23:41 +0100

    Fix copyright in otel source trashed by vscode
    Signed-off-by: Darach Ennis <darach@gmail.com>

--- src/connectors/otel.rs
+++ src/connectors/otel.rs
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, The T key: (), value: () key: (), value: ()remor Team
+// Copyright 2020-2021, The Tremor Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
