4884780378452f2176c0928807d2c111d167617c
Heinz N. Gies <heinz@licenser.net>
2021-04-06 09:39:57 +0200

    Reduce prop induced test coverage fluctuation
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tremor-value/src/value.rs
+++ tremor-value/src/value.rs
@@ -623,6 +623,64 @@ mod test {
         assert!(v1 != v2);
         assert!(v2 != v1);
     }
+
+    #[test]
+    fn nuber_ord_u64_i64() {
+        use std::cmp::Ordering;
+        assert_eq!(Value::from(1u64).cmp(&Value::from(1i64)), Ordering::Equal);
+        assert_eq!(Value::from(1i64).cmp(&Value::from(1u64)), Ordering::Equal);
+
+        assert_eq!(Value::from(2u64).cmp(&Value::from(1i64)), Ordering::Greater);
+        assert_eq!(Value::from(2i64).cmp(&Value::from(1u64)), Ordering::Greater);
+        assert_eq!(
+            Value::from(2u64).cmp(&Value::from(-1i64)),
+            Ordering::Greater
+        );
+
+        assert_eq!(Value::from(0u64).cmp(&Value::from(1i64)), Ordering::Less);
+        assert_eq!(Value::from(-1i64).cmp(&Value::from(1u64)), Ordering::Less);
+    }
+
+    #[test]
+    fn nuber_ord_u64_f64() {
+        use std::cmp::Ordering;
+        assert_eq!(Value::from(1u64).cmp(&Value::from(1f64)), Ordering::Equal);
+        assert_eq!(Value::from(1f64).cmp(&Value::from(1u64)), Ordering::Equal);
+
+        assert_eq!(Value::from(2u64).cmp(&Value::from(1f64)), Ordering::Greater);
+        assert_eq!(Value::from(2f64).cmp(&Value::from(1u64)), Ordering::Greater);
+        assert_eq!(
+            Value::from(2u64).cmp(&Value::from(-1f64)),
+            Ordering::Greater
+        );
+
+        assert_eq!(Value::from(0u64).cmp(&Value::from(1f64)), Ordering::Less);
+        assert_eq!(Value::from(-1f64).cmp(&Value::from(1u64)), Ordering::Less);
+    }
+
+    #[test]
+    fn nuber_ord_f64_u64() {
+        use std::cmp::Ordering;
+        assert_eq!(Value::from(1f64).cmp(&Value::from(1i64)), Ordering::Equal);
+        assert_eq!(Value::from(1i64).cmp(&Value::from(1f64)), Ordering::Equal);
+
+        assert_eq!(Value::from(2f64).cmp(&Value::from(1i64)), Ordering::Greater);
+        assert_eq!(Value::from(2i64).cmp(&Value::from(1f64)), Ordering::Greater);
+        assert_eq!(
+            Value::from(2f64).cmp(&Value::from(-1i64)),
+            Ordering::Greater
+        );
+
+        assert_eq!(Value::from(0f64).cmp(&Value::from(1i64)), Ordering::Less);
+        assert_eq!(Value::from(-1i64).cmp(&Value::from(1f64)), Ordering::Less);
+    }
+
+    #[test]
+    fn is_custom() {
+        assert!(Value::Bytes(vec![42u8].into()).is_custom());
+        assert!(!Value::from(1).is_custom());
+    }
+
     #[test]
     fn char_access() {
         let v = Value::from(json!({"snot": "🦡"}));
