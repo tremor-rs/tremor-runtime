b0d7c797f10e78d98a70cc29304818ce3b976848
Gary White Jr <gwhite@wayfair.com>
2021-05-06 17:17:50 -0400

    woopsie format
    Signed-off-by: Gary White Jr <gwhite@wayfair.com>

--- build.rs
+++ build.rs
@@ -12,50 +12,49 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 fn get_git_branch() -> String {
-  use std::process::Command;
+    use std::process::Command;
 
-  let branch = Command::new("git")
-      .arg("rev-parse")
-      .arg("--abbrev-ref")
-      .arg("HEAD")
-      .output();
-  if let Ok(branch_output) = branch {
-      let branch_string = String::from_utf8_lossy(&branch_output.stdout);
-      return branch_string.lines().next().unwrap_or("unknown_branch").to_string();
-  } else {
-      panic!("Can not get git branch: {}", branch.unwrap_err());
-  }
+    let branch = Command::new("git")
+        .arg("rev-parse")
+        .arg("--abbrev-ref")
+        .arg("HEAD")
+        .output();
+    if let Ok(branch_output) = branch {
+        let branch_string = String::from_utf8_lossy(&branch_output.stdout);
+        return branch_string
+            .lines()
+            .next()
+            .unwrap_or("unknown_branch")
+            .to_string();
+    } else {
+        panic!("Can not get git branch: {}", branch.unwrap_err());
+    }
 }
 
 fn get_git_commit() -> String {
-  use std::process::Command;
+    use std::process::Command;
 
-  let commit = Command::new("git")
-      .arg("rev-parse")
-      .arg("--verify")
-      .arg("HEAD")
-      .output();
+    let commit = Command::new("git")
+        .arg("rev-parse")
+        .arg("--verify")
+        .arg("HEAD")
+        .output();
 
-  if let Ok(commit_output) = commit {
-      let commit_string = String::from_utf8_lossy(&commit_output.stdout);
-      return commit_string.lines().next().unwrap_or("unknown_hash").to_string();
-  } else {
-      panic!("Can not get git commit: {}", commit.unwrap_err());
-  }
+    if let Ok(commit_output) = commit {
+        let commit_string = String::from_utf8_lossy(&commit_output.stdout);
+        return commit_string
+            .lines()
+            .next()
+            .unwrap_or("unknown_hash")
+            .to_string();
+    } else {
+        panic!("Can not get git commit: {}", commit.unwrap_err());
+    }
 }
 
 fn main() {
-  println!(
-    "cargo:rerun-if-changed=.git"
-  );
-  println!(
-    "cargo:rustc-env=VERSION_BRANCH={}", 
-    get_git_branch()
-  );
-  println!(
-    "cargo:rustc-env=VERSION_HASH={}", 
-    get_git_commit()
-  );
+    println!("cargo:rerun-if-changed=.git");
+    println!("cargo:rustc-env=VERSION_BRANCH={}", get_git_branch());
+    println!("cargo:rustc-env=VERSION_HASH={}", get_git_commit());
 }
