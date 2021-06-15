82555d57c16b8502c2c8648be734facc4d7fe868
Heinz N. Gies <heinz@licenser.net>
2021-06-15 10:58:58 +0200

    Exclude to_static from coverage
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- tremor-script/src/ast/to_static.rs
+++ tremor-script/src/ast/to_static.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This is just one big identify function with a few Cow::owned in it
+#![cfg(not(tarpaulin_include))]
+
 use beef::Cow;
 
 use crate::CustomFn;
