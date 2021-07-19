1b8507e7dbedb6a52fb5735e49c9db459daae9ae
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2021-07-19 18:21:26 +0200

    Update tremor-script/src/srs.rs
    Co-authored-by: Heinz N. Gies <heinz@licenser.net>

--- tremor-script/src/srs.rs
+++ tremor-script/src/srs.rs
@@ -609,17 +609,19 @@ impl EventPayload {
         // READ: ORDER MATTERS!
         self.raw.append(&mut other.raw);
 
-        // ALLOW: we are turning a longer lifetime into a shorter one for a
-        // covariant type ValueAndMeta. &mut is invariant over it's lifetime,
-        // but we are choosing a shorter one and passing it down not up. So a
-        // user should not be able to choose an inappropriate lifetime for it,
-        // plus they don't control the owner here.
+        //  we are turning a longer lifetime into a shorter one for a covariant
+        // type ValueAndMeta. &mut is invariant over it's lifetime, but we are
+        // choosing a shorter one and passing it down not up. So a user should
+        // not be able to choose an inappropriate lifetime for it, plus they
+        // don't control the owner here.
         join_f(
             unsafe {
+                // ALLOW: See above for explenation            
                 mem::transmute::<&'iref mut ValueAndMeta<'static>, &'iref mut ValueAndMeta<'iref>>(
                     &mut self.data,
                 )
             },
+            // ALLOW: See above for explenation
             unsafe { mem::transmute::<ValueAndMeta<'static>, ValueAndMeta<'iref>>(other.data) },
         )
     }
