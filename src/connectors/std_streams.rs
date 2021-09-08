4f2846f77dc64f8ae18a86a2e0f03988b9da03d0
Matthias Wahl <mwahl@wayfair.com>
2021-09-08 11:11:31 +0200

    Add quiescence beacon
    for detecting if we should stop reading from / writing to external connections.
    
    Not used yet. See next PR.
    
    Signed-off-by: Matthias Wahl <mwahl@wayfair.com>
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- src/connectors/std_streams.rs
+++ src/connectors/std_streams.rs
@@ -195,6 +195,7 @@ impl Connector for StdStreamConnector {
         &mut self,
         _ctx: &ConnectorContext,
         _notifier: ConnectionLostNotifier,
+        _quiescence: &QuiescenceBeacon,
     ) -> Result<bool> {
         Ok(true)
     }
