d79efdf78446bbdcb908eea0a9a19a3e393400a5
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-11-03 16:09:22 +0100

    Fix conversions for Value

--- src/pdk/value.rs
+++ src/pdk/value.rs
@@ -1,6 +1,3 @@
-//! This module implements a FFI-safe `Value` type to communicate with the
-//! plugins.
-
 use abi_stable::{
     std_types::{RBox, RCow, RHashMap, RVec},
     StableAbi,
@@ -12,6 +9,9 @@ pub type Object<'value> = RHashMap<RCow<'value, str>, Value<'value>>;
 /// Bytes
 pub type Bytes<'value> = RCow<'value, [u8]>;
 
+/// FFI-safe `Value` type to communicate with the plugins. It's meant to be
+/// converted to/from the original `tremor_value::Value` type and back so that
+/// it can be passed through the plugin interface.
 #[repr(C)]
 #[derive(StableAbi)]
 pub enum Value<'value> {
@@ -27,15 +27,42 @@ pub enum Value<'value> {
     Bytes(Bytes<'value>),
 }
 
-/// Easy conversions from the regular value to the PDK value
+/// Easily converting the regular value into the PDK value and back.
 impl<'value> From<tremor_value::Value<'value>> for Value<'value> {
-    fn from(original: tremor_value::Value) -> Self {
+    fn from(original: tremor_value::Value<'value>) -> Self {
         match original {
-            tremor_value::Value::Static(s) => Value::Static(s.into()),
-            tremor_value::Value::String(s) => Value::String(s.into()),
-            tremor_value::Value::Array(a) => Value::Array(a.into()),
-            tremor_value::Value::Object(m) => Value::Object(m.into()),
-            tremor_value::Value::Bytes(b) => Value::Bytes(b.into()),
+            // No conversion needed; `StaticNode` implements `StableAbi`
+            tremor_value::Value::Static(s) => Value::Static(s),
+            // This conversion is cheap
+            tremor_value::Value::String(s) => Value::String(conv_str(s)),
+            // This unfortunately requires iterating the array
+            tremor_value::Value::Array(a) => {
+                let a = a.into_iter().map(Into::into).collect();
+                Value::Array(a)
+            }
+            // This unfortunately requires iterating the map and a new
+            // allocation
+            tremor_value::Value::Object(m) => {
+                let m = m
+                    .into_iter()
+                    .map(|(k, v)| (conv_str(k), v.into()))
+                    .collect();
+                Value::Object(RBox::new(m))
+            }
+            // This conversion is cheap
+            tremor_value::Value::Bytes(b) => Value::Bytes(conv_u8(b)),
         }
     }
+}
+
+/// There are no direct conversions between `beef::Cow` and `RCow`, so the type
+/// has to be converted to std as the intermediate. These conversions are cheap
+/// and they shouldn't be a performance issue.
+fn conv_str(cow: beef::Cow<str>) -> RCow<str> {
+    let cow: std::borrow::Cow<str> = cow.into();
+    cow.into()
+}
+fn conv_u8(cow: beef::Cow<[u8]>) -> RCow<[u8]> {
+    let cow: std::borrow::Cow<[u8]> = cow.into();
+    cow.into()
 }
