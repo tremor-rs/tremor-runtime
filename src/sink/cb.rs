79b4182154278d0aac6a54f2989b8bf5f1ee61cc
Matthias Wahl <mwahl@wayfair.com>
2021-11-03 11:15:56 +0100

    Fix early timeout of quiescence process, not stopping instances.
    Signed-off-by: Matthias Wahl <mwahl@wayfair.com>

--- src/sink/cb.rs
+++ src/sink/cb.rs
@@ -43,7 +43,6 @@ impl Sink for Cb {
     ) -> ResultVec {
         let mut res = Vec::with_capacity(event.len());
         for (value, meta) in event.value_meta_iter() {
-            //            let event = event.clone();
             debug!(
                 "[Sink::CB] {} {}",
                 &event.id.event_id(),
