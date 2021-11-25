de33036f9ac3852be05c626c3709f6b0a218a62f
Matthias Wahl <mwahl@wayfair.com>
2021-11-25 11:54:56 +0100

    Test another error scenario for ES connector.
    Signed-off-by: Matthias Wahl <mwahl@wayfair.com>
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- tests/connector_elastic.rs
+++ tests/connector_elastic.rs
@@ -32,7 +32,7 @@ use tremor_common::url::ports::IN;
 use tremor_pipeline::{CbAction, Event, EventId};
 use tremor_runtime::errors::{Error, Result};
 use tremor_value::{literal, value::StaticValue};
-use value_trait::ValueAccess;
+use value_trait::{Mutable, Value, ValueAccess};
 
 const ELASTICSEARCH_VERSION: &'static str = "7.14.2";
 
@@ -94,6 +94,10 @@ async fn connector_elastic() -> Result<()> {
         r#"
 id: my_elastic
 type: elastic
+reconnect:
+  custom:
+    interval_ms: 1000
+    max_retries: 10
 config:
   nodes:
     - "http://127.0.0.1:{}"
@@ -226,8 +230,9 @@ config:
             }
         }
     });
+    let batched_id = EventId::new(0, 0, 1, 1);
     let event_batched = Event {
-        id: EventId::new(0, 0, 1, 1),
+        id: batched_id.clone(),
         is_batch: true,
         transactional: true,
         data: (batched_data, batched_meta).into(),
@@ -235,7 +240,54 @@ config:
     };
     harness.send_to_sink(event_batched, IN).await?;
     let out_event1 = out.get_event().await?;
-    dbg!(out_event1);
+    let mut meta = out_event1.data.suffix().meta().clone_static();
+    // remove _id
+    assert!(meta
+        .get_mut("connector")
+        .expect("no connector in meta")
+        .get_mut("elastic")
+        .expect("no elastic in meta")
+        .remove("_id")?
+        .map(|v| v.is_str())
+        .unwrap_or_default());
+    assert_eq!(
+        literal!({
+            "connector": {
+                "elastic": {
+                    "_index": "my_index",
+                    "_type": "_doc",
+                    "version": 1,
+                    "action": "index",
+                    "success": true
+                }
+            }
+        }),
+        meta
+    );
+    let mut data = out_event1.data.suffix().value().clone_static();
+    // remove _id
+    data.get_mut("index")
+        .expect("no index in data")
+        .remove("_id")?;
+    assert_eq!(
+        literal!({
+            "index": {
+                "_primary_term": 1,
+                "_shards": {
+                    "total": 2,
+                    "successful": 1,
+                    "failed": 0
+                },
+                "_type": "_doc",
+                "_index": "my_index",
+                "result": "created",
+                "_seq_no": 1,
+                "status": 201,
+                "_version": 1
+            }
+        }),
+        data
+    );
     let err_event2 = err.get_event().await?;
     let meta = err_event2.data.suffix().meta();
     assert_eq!(
@@ -247,7 +299,7 @@ config:
                     "_type": "_doc",
                     "action": "update",
                     "success": false
-                }
+                },
             }
         }),
         meta
@@ -258,12 +310,26 @@ config:
     let reason = data.get_str("reason");
     assert_eq!(Some("failed to parse field [field3] of type [boolean] in document with id '123'. Preview of field's value: '12'"), reason);
     let out_event3 = out.get_event().await?;
-    let meta = out_event3.data.suffix().meta();
+    let mut meta = out_event3.data.suffix().meta().clone_static();
+    // remove _id, as it is random
+    assert!(meta
+        .get_mut("connector")
+        .expect("No connector in meta")
+        .get_mut("elastic")
+        .expect("No elastic in meta")
+        .remove("_id")?
+        .map(|s| s.is_str()) // at least check that it was a string
+        .unwrap_or_default());
+
     assert_eq!(
         literal!({
             "connector": {
                 "elastic": {
-                    "success": true
+                    "success": true,
+                    "_index": "my_index",
+                    "_type": "_doc",
+                    "version": 1,
+                    "action": "index"
                 }
             },
             "correlation": "snot"
@@ -274,6 +340,46 @@ config:
     // a transactional event triggered a GD ACK
     let cf = in_pipe.get_contraflow().await?;
     assert_eq!(CbAction::Ack, cf.cb);
+    assert_eq!(batched_id, cf.id);
+
+    // check what happens when ES isnt reachable
+    container.stop();
+
+    let event = Event {
+        id: EventId::new(0, 0, 2, 2),
+        transactional: true,
+        data: (
+            literal!({}),
+            literal!({
+                "connector": {
+                    "elastic": {
+                        "_index": "my_index",
+                        "action": "delete",
+                        "_id": "123"
+                    }
+                },
+                "correlation": [true, false]
+            }),
+        )
+            .into(),
+        ..Event::default()
+    };
+    harness.send_to_sink(event.clone(), IN).await?;
+    let cf = in_pipe.get_contraflow().await?;
+    assert_eq!(CbAction::Fail, cf.cb);
+    let err_event = err.get_event().await?;
+    assert_eq!(
+        literal!({
+            "connector": {
+                "elastic": {
+                    "success": false
+                }
+            },
+            "error": "error sending request for url (http://127.0.0.1:9200/my_index/_bulk): error trying to connect: tcp connect error: Connection refused (os error 61)",
+            "correlation": [true, false]
+        }),
+        err_event.data.suffix().meta()
+    );
 
     let (out_events, err_events) = harness.stop().await?;
     assert!(out_events.is_empty());
