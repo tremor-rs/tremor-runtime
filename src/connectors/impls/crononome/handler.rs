22e80b07f45c22507d335d854c6a8767199dd5c7
Darach Ennis <dennis@wayfair.com>
2021-12-01 13:33:59 +0100

    Add crononome connector
    Signed-off-by: Darach Ennis <dennis@wayfair.com>
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- src/connectors/impls/crononome/handler.rs
+++ src/connectors/impls/crononome/handler.rs
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, The Tremor Team
+// Copyright 2021, The Tremor Team
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,11 +11,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#![cfg(not(tarpaulin_include))]
 
-use crate::source::prelude::*;
+use crate::connectors::prelude::*;
 use chrono::{DateTime, Utc};
 use cron::Schedule;
+use serde_yaml::Value as YamlValue;
 use std::clone::Clone;
 use std::cmp::Reverse;
 use std::cmp::{Eq, Ord, Ordering, PartialEq, PartialOrd};
@@ -23,7 +23,6 @@ use std::collections::BinaryHeap;
 use std::convert::TryFrom;
 use std::fmt;
 use std::str::FromStr;
-use tremor_common::time::nanotime;
 use tremor_script::prelude::*;
 
 #[derive(Deserialize, Clone)]
@@ -60,6 +59,14 @@ impl TryFrom<CronEntry> for CronEntryInt {
         })
     }
 }
+impl TryFrom<Value<'static>> for CronEntryInt {
+    type Error = crate::errors::Error;
+    fn try_from(entry: Value) -> Result<Self> {
+        let entry: CronEntry = tremor_value::structurize(entry)?;
+        Self::try_from(entry)
+    }
+}
+
 impl std::fmt::Debug for CronEntryInt {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} - {}", self.name, self.expr)
@@ -75,50 +82,6 @@ impl std::fmt::Debug for CronEntry {
 impl PartialEq for CronEntry {
     fn eq(&self, other: &Self) -> bool {
         self.name.eq(&other.name) && self.expr.eq(&other.expr)
-    }
-}
-
-#[derive(Deserialize, Clone)]
-pub struct Config {
-    pub entries: Vec<CronEntry>,
-}
-
-impl ConfigImpl for Config {}
-
-#[derive(Clone)]
-pub struct Crononome {
-    pub config: Config,
-    origin_uri: EventOriginUri,
-    cq: ChronomicQueue,
-    onramp_id: TremorUrl,
-}
-impl std::fmt::Debug for Crononome {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Crononome")
-    }
-}
-
-pub(crate) struct Builder {}
-impl onramp::Builder for Builder {
-    fn from_config(&self, id: &TremorUrl, config: &Option<YamlValue>) -> Result<Box<dyn Onramp>> {
-        if let Some(config) = config {
-            let config: Config = Config::new(config)?;
-            let origin_uri = EventOriginUri {
-                scheme: "tremor-crononome".to_string(),
-                host: hostname(),
-                port: None,
-                path: vec![],
-            };
-
-            Ok(Box::new(Crononome {
-                origin_uri,
-                config,
-                onramp_id: id.clone(),
-                cq: ChronomicQueue::default(),
-            }))
-        } else {
-            Err("Missing config for crononome onramp".into())
-        }
     }
 }
 
@@ -232,7 +195,7 @@ impl<I> TemporalPriorityQueue<I> {
 }
 
 #[derive(Debug, Clone)]
-struct ChronomicQueue {
+pub struct ChronomicQueue {
     tpq: TemporalPriorityQueue<CronEntryInt>,
 }
 
@@ -270,64 +233,6 @@ impl ChronomicQueue {
     }
 }
 
-#[async_trait::async_trait()]
-impl Source for Crononome {
-    fn id(&self) -> &TremorUrl {
-        &self.onramp_id
-    }
-
-    async fn pull_event(&mut self, id: u64) -> Result<SourceReply> {
-        if let Some(trigger) = self.cq.next() {
-            let mut origin_uri = self.origin_uri.clone();
-            origin_uri.path.push(trigger.0.clone());
-
-            let mut data: Value<'static> = Value::object_with_capacity(4);
-            data.insert("onramp", "crononome")?;
-            data.insert("ingest_ns", nanotime())?;
-            data.insert("id", id)?;
-            let mut tr: Value<'static> = Value::object_with_capacity(2);
-            tr.insert("name", trigger.0)?;
-            if let Some(payload) = trigger.1 {
-                tr.insert("payload", payload)?;
-            }
-            data.insert("trigger", tr)?;
-            Ok(SourceReply::Structured {
-                origin_uri,
-                data: data.into(),
-            })
-        } else {
-            Ok(SourceReply::Empty(100))
-        }
-    }
-
-    async fn init(&mut self) -> Result<SourceState> {
-        for entry in &self.config.entries {
-            match CronEntryInt::try_from(entry.clone()) {
-                Ok(entry) => self.cq.enqueue(&entry),
-                Err(e) => {
-                    return Err(format!(
-                        "Bad configuration in crononome - expression {} is illegal: {}",
-                        entry.name, e
-                    )
-                    .into())
-                }
-            }
-        }
-        Ok(SourceState::Connected)
-    }
-}
-
-#[async_trait::async_trait]
-impl Onramp for Crononome {
-    async fn start(&mut self, config: OnrampConfig<'_>) -> Result<onramp::Addr> {
-        SourceManager::start(self.clone(), config).await
-    }
-
-    fn default_codec(&self) -> &str {
-        "json"
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -359,19 +264,17 @@ mod tests {
         let mut tpq = TemporalPriorityQueue::default();
         let n1 = TemporalItem {
             at: DateTime::from(std::time::SystemTime::UNIX_EPOCH), // Epoch
-            what: CronEntryInt::try_from(CronEntry {
-                name: "a".to_string(),
-                expr: "* * * * * * 1970".to_string(),
-                payload: None,
-            })?,
+            what: CronEntryInt::try_from(literal!({
+                "name": "a",
+                "expr": "* * * * * * 1970"
+            }))?,
         };
         let n2 = TemporalItem {
             at: Utc::now(),
-            what: CronEntryInt::try_from(CronEntry {
-                name: "b".to_string(),
-                expr: "* * * * * * *".to_string(),
-                payload: None,
-            })?,
+            what: CronEntryInt::try_from(literal!({
+                "name": "b",
+                "expr": "* * * * * * *"
+            }))?,
         };
 
         tpq.enqueue(n1.clone());
@@ -393,19 +296,17 @@ mod tests {
         let mut tpq = TemporalPriorityQueue::default();
         let n1 = TemporalItem {
             at: DateTime::from(std::time::SystemTime::UNIX_EPOCH), // Epoch
-            what: CronEntryInt::try_from(CronEntry {
-                name: "a".to_string(),
-                expr: "* * * * * * 1970".to_string(),
-                payload: None,
-            })?,
+            what: CronEntryInt::try_from(literal!({
+                "name": "a",
+                "expr": "* * * * * * 1970"
+            }))?,
         };
         let n2 = TemporalItem {
             at: Utc::now(),
-            what: CronEntryInt::try_from(CronEntry {
-                name: "b".to_string(),
-                expr: "* * * * * * *".to_string(),
-                payload: None,
-            })?,
+            what: CronEntryInt::try_from(literal!({
+                "name": "b",
+                "expr": "* * * * * * *"
+            }))?,
         };
 
         tpq.enqueue(n1.clone());
@@ -426,21 +327,18 @@ mod tests {
     pub fn test_cq_fill_drain_refill() -> Result<()> {
         let mut cq = ChronomicQueue::default();
         // Dates before Jan 1st 1970 are invalid
-        let n1 = CronEntryInt::try_from(CronEntry {
-            name: "a".to_string(),
-            expr: "* * * * * * 1970".to_string(), // Dates before UNIX epoch start invalid
-            payload: None,
-        })?;
-        let n2 = CronEntryInt::try_from(CronEntry {
-            name: "b".to_string(),
-            expr: "* * * * * * *".to_string(),
-            payload: None,
-        })?;
-        let n3 = CronEntryInt::try_from(CronEntry {
-            name: "c".to_string(),
-            expr: "* * * * * * 2038".to_string(), // limit is 2038 due to the 2038 problem ( we'll be retired so letting this hang wait! )
-            payload: None,
-        })?;
+        let n1 = CronEntryInt::try_from(literal!({
+            "name": "a",
+            "expr": "* * * * * * 1970", // Dates before UNIX epoch start invalid
+        }))?;
+        let n2 = CronEntryInt::try_from(literal!({
+            "name": "b",
+            "expr": "* * * * * * *",
+        }))?;
+        let n3 = CronEntryInt::try_from(literal!({
+            "name": "c",
+            "expr": "* * * * * * 2038", // limit is 2038 due to the 2038 problem ( we'll be retired so letting this hang wait! )
+        }))?;
 
         cq.enqueue(&n1);
         cq.enqueue(&n2);
@@ -461,21 +359,18 @@ mod tests {
     pub fn test_cq_fill_pop_refill() -> Result<()> {
         let mut cq = ChronomicQueue::default();
         // Dates before Jan 1st 1970 are invalid
-        let n1 = CronEntryInt::try_from(CronEntry {
-            name: "a".to_string(),
-            expr: "* * * * * * 1970".to_string(), // Dates before UNIX epoch start invalid
-            payload: None,
-        })?;
-        let n2 = CronEntryInt::try_from(CronEntry {
-            name: "b".to_string(),
-            expr: "* * * * * * *".to_string(),
-            payload: None,
-        })?;
-        let n3 = CronEntryInt::try_from(CronEntry {
-            name: "c".to_string(),
-            expr: "* * * * * * 2038".to_string(), // limit is 2038 due to the 2038 problem ( we'll be retired so letting this hang wait! )
-            payload: None,
-        })?;
+        let n1 = CronEntryInt::try_from(literal!({
+            "name": "a",
+            "expr": "* * * * * * 1970" // Dates before UNIX epoch start invalid
+        }))?;
+        let n2 = CronEntryInt::try_from(literal!({
+            "name": "b".to_string(),
+            "expr": "* * * * * * *".to_string()
+        }))?;
+        let n3 = CronEntryInt::try_from(literal!({
+            "name": "c",
+            "expr": "* * * * * * 2038" // limit is 2038 due to the 2038 problem ( we'll be retired so letting this hang wait! )
+        }))?;
 
         cq.enqueue(&n1);
         cq.enqueue(&n2);
