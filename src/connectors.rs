f558c8c8c29ed3a27a2f3a138102c626486ad469
Daksh Chauhan <dak-x@outlook.com>
2022-01-10 20:30:05 +0530

    S3-Source.
    Signed-off-by: Daksh Chauhan <dak-x@outlook.com>

--- src/connectors.rs
+++ src/connectors.rs
@@ -1158,6 +1158,7 @@ pub fn builtin_connector_types() -> Vec<Box<dyn ConnectorBuilder + 'static>> {
         Box::new(impls::elastic::Builder::default()),
         Box::new(impls::crononome::Builder::default()),
         Box::new(impls::s3::Builder::default()),
+        Box::new(impls::s3_source::Builder::default()),
         Box::new(impls::kafka::consumer::Builder::default()),
     ]
 }
