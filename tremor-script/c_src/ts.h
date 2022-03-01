479a054c3c3c55112a4900305a3fa356e346dbd9
Matthias Wahl <matthiaswahl@m7w3.de>
2022-03-01 21:19:10 +0100

    Make tremor-script eqc tests run again.
    The hack of replacing all floats with 42 when doing erlang testing got lost, lol.
    
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- tremor-script/c_src/ts.h
+++ tremor-script/c_src/ts.h
@@ -1,3 +1,3 @@
 #pragma once
 
-uint64_t tremor_script_c_eval(unsigned char *ts_script, char unsigned *ts_result, uint64_t);
+uint64_t tremor_script_c_eval(unsigned char *ts_script, unsigned char *ts_result, uint64_t);
