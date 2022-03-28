487733bc9aeb34489341e227ed424b3b4ae69a04
Heinz N. Gies <heinz@licenser.net>
2022-03-28 13:22:59 +0200

    Update s3 tests
    Signed-off-by: Heinz N. Gies <heinz@licenser.net>

--- src/connectors/tests/s3.rs
+++ src/connectors/tests/s3.rs
@@ -14,14 +14,21 @@
 mod reader;
 mod writer;
 
-use crate::errors::Result;
+use crate::errors::{Error, Result};
 use async_std::stream::StreamExt;
 use async_std::task::JoinHandle;
+use aws_sdk_s3::{Client, Config, Credentials, Endpoint, Region};
+use rand::{distributions::Alphanumeric, Rng};
 use signal_hook::consts::{SIGINT, SIGQUIT, SIGTERM};
 use signal_hook_async_std::{Handle, Signals};
-use std::collections::HashMap;
-use testcontainers::clients;
+use std::{
+    collections::HashMap,
+    time::{Duration, Instant},
+};
 use testcontainers::Docker;
+use testcontainers::{clients, images::generic::GenericImage, Container, RunArgs};
+
+use super::free_port::find_free_tcp_port;
 /// Keeps track of process env manipulations and restores previous values upon drop
 pub(crate) struct EnvHelper {
     restore: HashMap<String, String>,
@@ -89,4 +96,66 @@ impl Drop for SignalHandler {
             async_std::task::block_on(s.cancel());
         }
     }
+}
+
+async fn wait_for_s3mock(port: u16) -> Result<()> {
+    let s3_client: Client = get_client(port);
+
+    let wait_for = Duration::from_secs(60);
+    let start = Instant::now();
+
+    while let Err(e) = s3_client.list_buckets().send().await {
+        if start.elapsed() > wait_for {
+            return Err(Error::from(e).chain_err(|| "Waiting for mock-s3 container timed out"));
+        }
+
+        async_std::task::sleep(Duration::from_secs(1)).await;
+    }
+    Ok(())
+}
+
+async fn spawn_docker<'d, D: Docker>(
+    docker: &'d D,
+    image: GenericImage,
+) -> (Container<'d, D, GenericImage>, u16, u16) {
+    let http_port = find_free_tcp_port().await.unwrap_or(10080);
+    let https_port = find_free_tcp_port().await.unwrap_or(10443);
+    let container = docker.run_with_args(
+        image,
+        RunArgs::default()
+            .with_mapped_port((http_port, 9090_u16))
+            .with_mapped_port((https_port, 9191_u16)),
+    );
+
+    (container, http_port, https_port)
+}
+
+fn random_bucket_name(prefix: &str) -> String {
+    format!(
+        "{}-{}",
+        prefix,
+        rand::thread_rng()
+            .sample_iter(Alphanumeric)
+            .map(char::from)
+            .take(10)
+            .collect::<String>()
+    )
+}
+
+fn get_client(http_port: u16) -> Client {
+    let s3_config = Config::builder()
+        .credentials_provider(Credentials::new(
+            "KEY_NOT_REQD",
+            "KEY_NOT_REQD",
+            None,
+            None,
+            "Environment",
+        ))
+        .region(Region::new("ap-south-1"))
+        .endpoint_resolver(Endpoint::immutable(
+            format!("http://localhost:{http_port}").parse().unwrap(),
+        ))
+        .build();
+
+    Client::from_conf(s3_config)
 }
