a6869021ab746b384ec8ede55b37d82fe8688cf0
Ramona Luczkiewicz <rluczkiewicz@wayfair.com>
2022-04-22 14:06:30 +0200

    Add support for Struct and Timestamp types
    Signed-off-by: Ramona Luczkiewicz <rluczkiewicz@wayfair.com>

--- src/connectors/impls/gbq/sink.rs
+++ src/connectors/impls/gbq/sink.rs
@@ -7,8 +7,8 @@ use googapis::google::cloud::bigquery::storage::v1::append_rows_request::ProtoDa
 use googapis::google::cloud::bigquery::storage::v1::big_query_write_client::BigQueryWriteClient;
 use googapis::google::cloud::bigquery::storage::v1::{append_rows_request, write_stream, AppendRowsRequest, CreateWriteStreamRequest, ProtoRows, ProtoSchema, WriteStream, TableFieldSchema, table_field_schema};
 use googapis::google::cloud::bigquery::storage::v1::table_field_schema::Type as TableType;
+use prost::encoding::WireType;
 use prost_types::{field_descriptor_proto, DescriptorProto, FieldDescriptorProto};
-use prost_types::field_descriptor_proto::Type;
 use tonic::codegen::InterceptedService;
 use tonic::metadata::{Ascii, MetadataValue};
 use tonic::service::Interceptor;
@@ -36,8 +36,11 @@ impl Interceptor for AuthInterceptor {
 }
 
 struct Field {
-    grpc_type: field_descriptor_proto::Type,
-    tag: u32
+    table_type: TableType,
+    tag: u32,
+
+    // ignored if the table_type is not struct
+    subfields: HashMap<String, Field>
 }
 
 struct JsonToProtobufMapping {
@@ -45,66 +48,132 @@ struct JsonToProtobufMapping {
     descriptor: DescriptorProto
 }
 
-impl JsonToProtobufMapping {
-    pub fn new(vec: &Vec<TableFieldSchema>) -> Self {
-        let mut fields = HashMap::new();
-        let mut proto_fields = vec![];
+fn map_field(schema_name:&str, raw_fields:&Vec<TableFieldSchema>) -> (DescriptorProto, HashMap<String, Field>) {
+    let mut nested_types = vec![];
+    let mut proto_fields = vec![];
+    let mut fields = HashMap::new();
+    let mut tag = 1;
 
-        let mut tag:u32 = 1;
-        for raw_field in vec.iter() {
-            let r#type = match table_field_schema::Type::from_i32(raw_field.r#type) {
-                None => todo!("Unknown field type"),
-                Some(table_field_schema::Type::Int64) => field_descriptor_proto::Type::Int64,
-                Some(TableType::String) => field_descriptor_proto::Type::String,
-                Some(TableType::Double) => field_descriptor_proto::Type::Double,
-                Some(TableType::Bool) => field_descriptor_proto::Type::Bool,
-                Some(TableType::Bytes) => field_descriptor_proto::Type::Bytes,
+    for raw_field in raw_fields {
+        let mut type_name = None;
+        let mut subfields = HashMap::new();
 
-                Some(TableType::Date) |
-                Some(TableType::Time) |
-                Some(TableType::Datetime) |
-                Some(TableType::Geography) |
-                Some(TableType::Numeric) |
-                Some(TableType::Bignumeric) |
-                Some(TableType::Interval) |
-                Some(TableType::Json) | Some(TableType::Timestamp) | Some(TableType::Unspecified) | Some(TableType::Struct) => todo!("Unsupported field type")
-            };
+        let grpc_type = match table_field_schema::Type::from_i32(raw_field.r#type) {
+            None => todo!("Unknown field type"),
+            Some(table_field_schema::Type::Int64) => field_descriptor_proto::Type::Int64,
+            Some(TableType::String) => field_descriptor_proto::Type::String,
+            Some(TableType::Double) => field_descriptor_proto::Type::Double,
+            Some(TableType::Bool) => field_descriptor_proto::Type::Bool,
+            Some(TableType::Bytes) => field_descriptor_proto::Type::Bytes,
 
-            proto_fields.push(FieldDescriptorProto {
-                name: Some(raw_field.name.to_string()),
-                number: Some(i32::try_from(tag).unwrap()),
-                label: None,
-                r#type: Some(i32::from(r#type)),
-                type_name: None,
-                extendee: None,
-                default_value: None,
-                oneof_index: None,
-                json_name: None,
-                options: None,
-                proto3_optional: None,
-            });
+            // YYYY-[M]M-[D]D
+            Some(TableType::Date) => field_descriptor_proto::Type::String,
+            // [H]H:[M]M:[S]S[.DDDDDD|.F]
+            Some(TableType::Time) => field_descriptor_proto::Type::String,
+            // YYYY-[M]M-[D]D[( |T)[H]H:[M]M:[S]S[.F]]
+            Some(TableType::Datetime) => field_descriptor_proto::Type::String,
+            // fixme is this a json string or what?
+            Some(TableType::Geography) => todo!("Need to support geography"),
+            // String, because it's a precise, f32/f64 would lose precision
+            Some(TableType::Numeric) => field_descriptor_proto::Type::String,
+            Some(TableType::Bignumeric) => field_descriptor_proto::Type::String,
+            // [sign]Y-M [sign]D [sign]H:M:S[.F]
+            Some(TableType::Interval) => field_descriptor_proto::Type::String,
+            Some(TableType::Json) => field_descriptor_proto::Type::String,
+            // YYYY-[M]M-[D]D[( |T)[H]H:[M]M:[S]S[.F]][time zone]
+            Some(TableType::Timestamp) => field_descriptor_proto::Type::String,
+            Some(TableType::Struct) => {
+                let type_name_for_field = format!("struct_{}", raw_field.name);
+                let mapped = map_field(&type_name_for_field, &raw_field.fields);
+                nested_types.push(mapped.0);
+                subfields = mapped.1;
 
-            fields.insert(raw_field.name.to_string(), Field {tag, grpc_type: r#type });
+                type_name = Some(type_name_for_field);
+                field_descriptor_proto::Type::Message
+            },
 
-            tag += 1;
-        }
-
-        let descriptor = DescriptorProto {
-            name: Some("row".to_string()),
-            field: proto_fields,
-            extension: vec![],
-            nested_type: vec![],
-            enum_type: vec![],
-            extension_range: vec![],
-            oneof_decl: vec![],
-            options: None,
-            reserved_range: vec![],
-            reserved_name: vec![],
+            // fixme this should log an error and ignore the field, the type is supposed to never be unspecified
+            Some(TableType::Unspecified) => todo!("Unsupported field type")
         };
 
+        proto_fields.push(FieldDescriptorProto {
+            name: Some(raw_field.name.to_string()),
+            number: Some(i32::try_from(tag).unwrap()),
+            label: None,
+            r#type: Some(i32::from(grpc_type)),
+            type_name,
+            extendee: None,
+            default_value: None,
+            oneof_index: None,
+            json_name: None,
+            options: None,
+            proto3_optional: None,
+        });
+
+
+        let table_type = table_field_schema::Type::from_i32(raw_field.r#type).unwrap();
+        fields.insert(raw_field.name.to_string(), Field { tag, table_type, subfields });
+
+        tag += 1;
+    }
+
+    (DescriptorProto {
+        name: Some(schema_name.to_string()),
+        field: proto_fields,
+        extension: vec![],
+        nested_type: nested_types,
+        enum_type: vec![],
+        extension_range: vec![],
+        oneof_decl: vec![],
+        options: None,
+        reserved_range: vec![],
+        reserved_name: vec![],
+    }, fields)
+}
+
+fn encode_field(val: &Value, field:&Field, result:&mut Vec<u8>) {
+    let tag = field.tag;
+
+    // fixme check which fields are required and fail if they're missing
+    // fixme do not panic if the tremor type does not match
+    match field.table_type {
+        TableType::Double => prost::encoding::double::encode(tag, &val.as_f64().unwrap(), result),
+        TableType::Int64 => prost::encoding::int64::encode(tag, &val.as_i64().unwrap(), result),
+        TableType::Bool => prost::encoding::bool::encode(tag, &val.as_bool().unwrap(), result),
+        TableType::String
+        | TableType::Date
+        | TableType::Time
+        | TableType::Datetime
+        | TableType::Timestamp => prost::encoding::string::encode(tag, &val.as_str().unwrap().to_string(), result),
+        TableType::Struct => {
+            let mut struct_buf:Vec<u8> = vec![];
+            for (k,v) in val.as_object().unwrap() {
+                let subfield_description = field.subfields.get(&k.to_string()).unwrap();
+                encode_field(v, subfield_description, &mut struct_buf);
+            }
+            prost::encoding::encode_key(tag, WireType::LengthDelimited, result);
+            prost::encoding::encode_varint(struct_buf.len() as u64, result);
+            result.append(&mut struct_buf);
+        },
+
+        // fixme handle those types!
+        TableType::Unspecified => {}
+        TableType::Bytes => {}
+        TableType::Geography => {}
+        TableType::Numeric => {}
+        TableType::Bignumeric => {}
+        TableType::Interval => {}
+        TableType::Json => {}
+    }
+}
+
+impl JsonToProtobufMapping {
+    pub fn new(vec: &Vec<TableFieldSchema>) -> Self {
+        let descriptor = map_field("table", vec);
+
         Self {
-            descriptor,
-            fields
+            descriptor: descriptor.0,
+            fields: descriptor.1,
         }
     }
 
@@ -113,21 +182,7 @@ impl JsonToProtobufMapping {
         if let Some(obj) = value.as_object() {
             for (key, val) in obj {
                 if let Some(field) = self.fields.get(&key.to_string()) {
-                    let tag = field.tag;
-
-                    // fixme check which fields are required and fail if they're missing
-                    // fixme do not panic if the tremor type does not match
-                    match field.grpc_type {
-                        Type::Double => prost::encoding::double::encode(tag, &val.as_f64().unwrap(), &mut result),
-                        Type::Float => prost::encoding::float::encode(tag, &val.as_f32().unwrap(), &mut result),
-                        Type::Int64 => prost::encoding::int64::encode(tag, &val.as_i64().unwrap(), &mut result),
-                        Type::Uint64 => prost::encoding::uint64::encode(tag, &val.as_u64().unwrap(), &mut result),
-                        Type::Int32 => prost::encoding::int32::encode(tag, &val.as_i32().unwrap(), &mut result),
-                        Type::Uint32 => prost::encoding::uint32::encode(tag, &val.as_u32().unwrap(), &mut result),
-                        Type::Bool => prost::encoding::bool::encode(tag, &val.as_bool().unwrap(), &mut result),
-                        Type::String => prost::encoding::string::encode(tag, &val.as_str().unwrap().to_string(), &mut result),
-                        Type::Bytes | Type::Sint64 | Type::Sint32 | Type::Sfixed64 | Type::Sfixed32 | Type::Enum | Type::Message | Type::Group | Type::Fixed32 | Type::Fixed64 => todo!("This type is not supported"),
-                    }
+                    encode_field(val, field, &mut result);
                 }
             }
         }
