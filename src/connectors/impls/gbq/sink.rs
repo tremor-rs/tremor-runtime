e0f2f9d8a9c4d8210e277924c7e907fcf1736362
Ramona Luczkiewicz <rluczkiewicz@wayfair.com>
2022-04-25 11:18:46 +0200

    Improve error handling
    Signed-off-by: Ramona Luczkiewicz <rluczkiewicz@wayfair.com>

--- src/connectors/impls/gbq/sink.rs
+++ src/connectors/impls/gbq/sink.rs
@@ -1,14 +1,17 @@
-use std::collections::HashMap;
 use crate::connectors::impls::gbq::Config;
 use crate::connectors::prelude::*;
 use async_std::prelude::StreamExt;
 use futures::stream;
 use googapis::google::cloud::bigquery::storage::v1::append_rows_request::ProtoData;
 use googapis::google::cloud::bigquery::storage::v1::big_query_write_client::BigQueryWriteClient;
-use googapis::google::cloud::bigquery::storage::v1::{append_rows_request, write_stream, AppendRowsRequest, CreateWriteStreamRequest, ProtoRows, ProtoSchema, WriteStream, TableFieldSchema, table_field_schema};
 use googapis::google::cloud::bigquery::storage::v1::table_field_schema::Type as TableType;
+use googapis::google::cloud::bigquery::storage::v1::{
+    append_rows_request, table_field_schema, write_stream, AppendRowsRequest,
+    CreateWriteStreamRequest, ProtoRows, ProtoSchema, TableFieldSchema, WriteStream,
+};
 use prost::encoding::WireType;
 use prost_types::{field_descriptor_proto, DescriptorProto, FieldDescriptorProto};
+use std::collections::HashMap;
 use tonic::codegen::InterceptedService;
 use tonic::metadata::{Ascii, MetadataValue};
 use tonic::service::Interceptor;
@@ -18,7 +21,7 @@ use tonic::{Request, Status};
 pub(crate) struct GbqSink {
     client: BigQueryWriteClient<InterceptedService<Channel, AuthInterceptor>>,
     write_stream: WriteStream,
-    mapping: JsonToProtobufMapping
+    mapping: JsonToProtobufMapping,
 }
 
 pub(crate) struct AuthInterceptor {
@@ -40,15 +43,18 @@ struct Field {
     tag: u32,
 
     // ignored if the table_type is not struct
-    subfields: HashMap<String, Field>
+    subfields: HashMap<String, Field>,
 }
 
 struct JsonToProtobufMapping {
     fields: HashMap<String, Field>,
-    descriptor: DescriptorProto
+    descriptor: DescriptorProto,
 }
 
-fn map_field(schema_name:&str, raw_fields:&Vec<TableFieldSchema>) -> (DescriptorProto, HashMap<String, Field>) {
+fn map_field(
+    schema_name: &str,
+    raw_fields: &Vec<TableFieldSchema>,
+) -> (DescriptorProto, HashMap<String, Field>) {
     let mut nested_types = vec![];
     let mut proto_fields = vec![];
     let mut fields = HashMap::new();
@@ -58,31 +64,39 @@ fn map_field(schema_name:&str, raw_fields:&Vec<TableFieldSchema>) -> (Descriptor
         let mut type_name = None;
         let mut subfields = HashMap::new();
 
-        let grpc_type = match table_field_schema::Type::from_i32(raw_field.r#type) {
-            None => todo!("Unknown field type"),
-            Some(table_field_schema::Type::Int64) => field_descriptor_proto::Type::Int64,
-            Some(TableType::String) => field_descriptor_proto::Type::String,
-            Some(TableType::Double) => field_descriptor_proto::Type::Double,
-            Some(TableType::Bool) => field_descriptor_proto::Type::Bool,
-            Some(TableType::Bytes) => field_descriptor_proto::Type::Bytes,
+        let table_type =
+            if let Some(table_type) = table_field_schema::Type::from_i32(raw_field.r#type) {
+                warn!("Found a field of unknown type: {}", raw_field.name);
+                table_type
+            } else {
+                continue;
+            };
 
+        let grpc_type = match table_type {
+            table_field_schema::Type::Int64 => field_descriptor_proto::Type::Int64,
+            TableType::Double => field_descriptor_proto::Type::Double,
+            TableType::Bool => field_descriptor_proto::Type::Bool,
+            TableType::Bytes => field_descriptor_proto::Type::Bytes,
+
+
+            TableType::String
             // YYYY-[M]M-[D]D
-            Some(TableType::Date) => field_descriptor_proto::Type::String,
+            | TableType::Date
             // [H]H:[M]M:[S]S[.DDDDDD|.F]
-            Some(TableType::Time) => field_descriptor_proto::Type::String,
+            | TableType::Time
             // YYYY-[M]M-[D]D[( |T)[H]H:[M]M:[S]S[.F]]
-            Some(TableType::Datetime) => field_descriptor_proto::Type::String,
+            | TableType::Datetime
             // The GEOGRAPHY type is based on the OGC Simple Features specification (SFS)
-            Some(TableType::Geography) => field_descriptor_proto::Type::String,
+            | TableType::Geography
             // String, because it's a precise, f32/f64 would lose precision
-            Some(TableType::Numeric) => field_descriptor_proto::Type::String,
-            Some(TableType::Bignumeric) => field_descriptor_proto::Type::String,
+            | TableType::Numeric
+            | TableType::Bignumeric
             // [sign]Y-M [sign]D [sign]H:M:S[.F]
-            Some(TableType::Interval) => field_descriptor_proto::Type::String,
-            Some(TableType::Json) => field_descriptor_proto::Type::String,
+            | TableType::Interval
+            | TableType::Json
             // YYYY-[M]M-[D]D[( |T)[H]H:[M]M:[S]S[.F]][time zone]
-            Some(TableType::Timestamp) => field_descriptor_proto::Type::String,
-            Some(TableType::Struct) => {
+            | TableType::Timestamp => field_descriptor_proto::Type::String,
+            TableType::Struct => {
                 let type_name_for_field = format!("struct_{}", raw_field.name);
                 let mapped = map_field(&type_name_for_field, &raw_field.fields);
                 nested_types.push(mapped.0);
@@ -90,15 +104,17 @@ fn map_field(schema_name:&str, raw_fields:&Vec<TableFieldSchema>) -> (Descriptor
 
                 type_name = Some(type_name_for_field);
                 field_descriptor_proto::Type::Message
-            },
+            }
 
-            // fixme this should log an error and ignore the field, the type is supposed to never be unspecified
-            Some(TableType::Unspecified) => todo!("Unsupported field type")
+            TableType::Unspecified => {
+                warn!("Found a field of unspecified type: {}", raw_field.name);
+                continue;
+            }
         };
 
         proto_fields.push(FieldDescriptorProto {
             name: Some(raw_field.name.to_string()),
-            number: Some(i32::try_from(tag).unwrap()),
+            number: Some(tag as i32),
             label: None,
             r#type: Some(i32::from(grpc_type)),
             type_name,
@@ -110,28 +126,36 @@ fn map_field(schema_name:&str, raw_fields:&Vec<TableFieldSchema>) -> (Descriptor
             proto3_optional: None,
         });
 
-
-        let table_type = table_field_schema::Type::from_i32(raw_field.r#type).unwrap();
-        fields.insert(raw_field.name.to_string(), Field { tag, table_type, subfields });
+        fields.insert(
+            raw_field.name.to_string(),
+            Field {
+                table_type,
+                tag,
+                subfields,
+            },
+        );
 
         tag += 1;
     }
 
-    (DescriptorProto {
-        name: Some(schema_name.to_string()),
-        field: proto_fields,
-        extension: vec![],
-        nested_type: nested_types,
-        enum_type: vec![],
-        extension_range: vec![],
-        oneof_decl: vec![],
-        options: None,
-        reserved_range: vec![],
-        reserved_name: vec![],
-    }, fields)
+    (
+        DescriptorProto {
+            name: Some(schema_name.to_string()),
+            field: proto_fields,
+            extension: vec![],
+            nested_type: nested_types,
+            enum_type: vec![],
+            extension_range: vec![],
+            oneof_decl: vec![],
+            options: None,
+            reserved_range: vec![],
+            reserved_name: vec![],
+        },
+        fields,
+    )
 }
 
-fn encode_field(val: &Value, field:&Field, result:&mut Vec<u8>) {
+fn encode_field(val: &Value, field: &Field, result: &mut Vec<u8>) {
     let tag = field.tag;
 
     // fixme check which fields are required and fail if they're missing
@@ -147,25 +171,33 @@ fn encode_field(val: &Value, field:&Field, result:&mut Vec<u8>) {
         | TableType::Timestamp
         | TableType::Numeric
         | TableType::Bignumeric
-        | TableType::Geography => prost::encoding::string::encode(tag, &val.as_str().unwrap().to_string(), result),
+        | TableType::Geography => {
+            prost::encoding::string::encode(tag, &val.as_str().unwrap().to_string(), result);
+        }
         TableType::Struct => {
-            let mut struct_buf:Vec<u8> = vec![];
-            for (k,v) in val.as_object().unwrap() {
+            let mut struct_buf: Vec<u8> = vec![];
+            for (k, v) in val.as_object().unwrap() {
                 let subfield_description = field.subfields.get(&k.to_string()).unwrap();
                 encode_field(v, subfield_description, &mut struct_buf);
             }
             prost::encoding::encode_key(tag, WireType::LengthDelimited, result);
             prost::encoding::encode_varint(struct_buf.len() as u64, result);
             result.append(&mut struct_buf);
-        },
-        TableType::Bytes => prost::encoding::bytes::encode(tag, &Vec::from(val.as_bytes().unwrap()), result),
+        }
+        TableType::Bytes => {
+            prost::encoding::bytes::encode(tag, &Vec::from(val.as_bytes().unwrap()), result);
+        }
 
         // fixme to test this we need a json field, which we don't have right now
-        TableType::Json => prost::encoding::string::encode(tag, &simd_json::to_string(val).unwrap(), result),
+        TableType::Json => {
+            prost::encoding::string::encode(tag, &simd_json::to_string(val).unwrap(), result);
+        }
         // fixme this is not GA, need to test
         TableType::Interval => {}
 
-        TableType::Unspecified => { warn!("Found a field of unspecified type - ignoring.")}
+        TableType::Unspecified => {
+            warn!("Found a field of unspecified type - ignoring.");
+        }
     }
 }
 
@@ -179,7 +211,7 @@ impl JsonToProtobufMapping {
         }
     }
 
-    pub fn map(&self, value:Value) -> Vec<u8> {
+    pub fn map(&self, value: &Value) -> Vec<u8> {
         let mut result = vec![];
         if let Some(obj) = value.as_object() {
             for (key, val) in obj {
@@ -192,13 +224,14 @@ impl JsonToProtobufMapping {
         result
     }
 
-    pub fn descriptor(&self) -> &DescriptorProto { &self.descriptor }
+    pub fn descriptor(&self) -> &DescriptorProto {
+        &self.descriptor
+    }
 }
-
 impl GbqSink {
     pub async fn new(config: Config) -> Result<Self> {
         let token_metadata_value =
-            MetadataValue::from_str(format!("Bearer {}", config.token).as_str()).unwrap();
+            MetadataValue::from_str(format!("Bearer {}", config.token).as_str())?;
 
         let tls_config = ClientTlsConfig::new()
             .ca_certificate(Certificate::from_pem(googapis::CERTIFICATES))
@@ -230,11 +263,18 @@ impl GbqSink {
             .await?
             .into_inner();
 
-        let mapping = JsonToProtobufMapping::new(&write_stream.table_schema.as_ref().unwrap().clone().fields);
+        let mapping = JsonToProtobufMapping::new(
+            &write_stream
+                .table_schema
+                .as_ref()
+                .ok_or(ErrorKind::GbqSinkFailed("Table schema was not provided"))?
+                .clone()
+                .fields,
+        );
         Ok(Self {
             client,
             write_stream,
-            mapping
+            mapping,
         })
     }
 }
@@ -258,7 +298,7 @@ impl Sink for GbqSink {
                     proto_descriptor: Some(self.mapping.descriptor().clone()),
                 }),
                 rows: Some(ProtoRows {
-                    serialized_rows: vec![self.mapping.map(event.data.parts().0.clone())],
+                    serialized_rows: vec![self.mapping.map(event.data.parts().0)],
                 }),
             })),
         };
@@ -269,15 +309,21 @@ impl Sink for GbqSink {
             .await?
             .into_inner();
 
-        while let Some(x) = apnd_response.next().await {
-            error!("{:?}", x);
-        }
+        if let Some(response) = apnd_response.next().await {
+            match response {
+                Ok(_) => Ok(SinkReply::ACK),
+                Err(e) => {
+                    error!("Failed to write event to BigQuery: {}", e);
 
-        Ok(SinkReply::NONE)
+                    Ok(SinkReply::FAIL)
+                }
+            }
+        } else {
+            Ok(SinkReply::FAIL)
+        }
     }
 
     fn auto_ack(&self) -> bool {
-        // FIXME we should NOT do auto ack, instead ACK when we get the response from GBQ
-        true
+        false
     }
 }
