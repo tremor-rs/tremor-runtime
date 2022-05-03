dd0fe24e99ea91173c3c1f8656950440ac52d48e
agares <ja@agares.info>
2022-05-03 09:50:41 +0000

    More tests
    Signed-off-by: Ramona Luczkiewicz <rluczkiewicz@wayfair.com>

--- tremor-script/src/std_lib/win.rs
+++ tremor-script/src/std_lib/win.rs
@@ -297,6 +297,27 @@ mod test {
     }
 
     #[test]
+    fn collect_merge() -> Result<()> {
+        let mut a = CollectFlattened::default();
+        let mut b = CollectFlattened::default();
+        a.init();
+        b.init();
+
+        a.accumulate(&[&Value::from(1)])?;
+        a.accumulate(&[&Value::from(2)])?;
+        b.accumulate(&[&Value::from(3)])?;
+
+        a.merge(&b)?;
+
+        let result = a.emit().unwrap();
+        assert_eq!(Value::from(1), result.get_idx(0).unwrap());
+        assert_eq!(Value::from(2), result.get_idx(1).unwrap());
+        assert_eq!(Value::from(3), result.get_idx(2).unwrap());
+
+        Ok(())
+    }
+
+    #[test]
     fn last_merge() -> Result<()> {
         let mut a = Last::default();
         let mut b = Last::default();
