6043f58bed1f414f27e78b3d3a54c8b84af4592f
PrimalPimmy <Prashant20.pm@gmail.com>
2022-05-08 20:02:35 +0530

    Version inserted correctly in tremor dependencies of some workspaces
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- .github/scripts/Bump.sh
+++ .github/scripts/Bump.sh
@@ -20,9 +20,28 @@ sed -e "s/^## Unreleased$/## [${new}]/" -i.release "CHANGELOG.md"
 
 echo "Updating tremor dependencies in tremor-script"
 cd tremor-script
-echo "Updating tremor-common"
 sed -e "s/^tremor-common = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
-echo "Updating tremor-influx"
 sed -e "s/^tremor-influx = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
-echo "Updating tremor-value"
 sed -e "s/^tremor-value = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+cd ..
+cd tremor-api
+echo "Updating tremor dependencies in tremor-api"
+sed -e "s/^tremor-runtime = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-common = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-value = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-script = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-pipeline = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+cd ..
+cd tremor-cli
+echo "Updating tremor dependencies in tremor-cli"
+sed -e "s/^tremor-runtime = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-common = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-value = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-script = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-pipeline = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-api = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+cd ..
+echo "Updating tremor dependencies in tremor-pipeline"
+sed -e "s/^tremor-common = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-value = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
+sed -e "s/^tremor-script = { version = \"${old}\"/tremor-common = { version = \"${new}\"/" -i.release "Cargo.toml"
