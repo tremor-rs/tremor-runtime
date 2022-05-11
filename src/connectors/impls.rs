8658b92b4837b54bdf18ce3d73975e70c1b12f68
Matthias Wahl <matthiaswahl@m7w3.de>
2022-05-11 12:54:30 +0200

    Remove no_coverage attribute
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- src/connectors/impls.rs
+++ src/connectors/impls.rs
@@ -47,7 +47,6 @@ pub(crate) mod otel;
 /// AWS S3 connectors
 pub(crate) mod s3;
 /// std streams connector (stdout, stderr, stdin)
-#[cfg_attr(coverage, no_coverage)]
 pub(crate) mod stdio;
 /// tcp server and client connector impls
 pub(crate) mod tcp;
