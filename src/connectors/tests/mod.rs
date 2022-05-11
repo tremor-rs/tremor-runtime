4d7aed7b843daf785dfb3a165a20fc219c640d37
Ramona Luczkiewicz <rluczkiewicz@wayfair.com>
2022-05-11 17:13:00 +0200

    More tests
    Signed-off-by: Ramona Luczkiewicz <rluczkiewicz@wayfair.com>

--- src/connectors/tests/mod.rs
+++ src/connectors/tests/mod.rs
@@ -26,6 +26,7 @@ mod file;
 mod file_non_existent;
 #[cfg(feature = "file-integration")]
 mod file_xz;
+mod gpubsub;
 #[cfg(feature = "http-integration")]
 mod http;
 #[cfg(feature = "kafka-integration")]
