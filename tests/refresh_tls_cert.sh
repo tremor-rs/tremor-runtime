e0b505615cece0242f30138da86a4248f62312e5
Matthias Wahl <matthiaswahl@m7w3.de>
2022-05-17 21:51:59 +0200

    Allow other users than myself to read our generates tls key
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- tests/refresh_tls_cert.sh
+++ tests/refresh_tls_cert.sh
@@ -7,3 +7,4 @@ pwd=`pwd`/tests
 if [ -f "$pwd/localhost.cert" ]; then  rm -f $pwd/localhost.cert; fi
 if [ -f "$pwd/localhost.key" ]; then rm $pwd/localhost.key; fi
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out $pwd/localhost.cert -keyout $pwd/localhost.key -subj /CN=localhost -config $pwd/openssl.cfg
+chmod 664 $pwd/localhost.key
