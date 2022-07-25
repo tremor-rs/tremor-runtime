596d65a187839bd24289979d313459f0056e046a
Ramona Łuczkiewicz <rluczkiewicz@wayfair.com>
2022-07-25 09:10:53 +0200

    Add comments
    Signed-off-by: Ramona Łuczkiewicz <rluczkiewicz@wayfair.com>

--- src/connectors/impls/gcs/api_client.rs
+++ src/connectors/impls/gcs/api_client.rs
@@ -1,3 +1,17 @@
+// Copyright 2020-2021, The Tremor Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::connectors::impls::gcs::chunked_buffer::BufferPart;
 use crate::connectors::prelude::{ContraflowData, ErrorKind, Result, Url};
 use crate::connectors::utils::url::HttpsDefaults;
@@ -21,7 +35,6 @@ pub(crate) trait ApiClient {
     ) -> Result<()>;
 }
 
-// FIXME: make the fields private, create a `new` that initialises
 pub(crate) struct DefaultApiClient<THttpClient: HttpClient> {
     #[cfg(not(test))]
     token: Token,
