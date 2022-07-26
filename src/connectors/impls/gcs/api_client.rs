f466bf9d6a69e9af5c09f35ad59e24180602b460
Ramona Łuczkiewicz <rluczkiewicz@wayfair.com>
2022-07-26 12:41:27 +0200

    More tests
    Signed-off-by: Ramona Łuczkiewicz <rluczkiewicz@wayfair.com>

--- src/connectors/impls/gcs/api_client.rs
+++ src/connectors/impls/gcs/api_client.rs
@@ -82,7 +82,7 @@ async fn retriable_request<
     make_request: TMakeRequest,
 ) -> Result<Response> {
     let max_retries = backoff_strategy.max_retries();
-    for i in 0..max_retries {
+    for i in 1..max_retries + 1 {
         let request = make_request();
         let error_wait_time = backoff_strategy.wait_time(i);
 
@@ -93,14 +93,14 @@ async fn retriable_request<
                 match result {
                     Ok(mut response) => {
                         if response.status().is_server_error() {
+                            let response_body = response
+                                .body_string()
+                                .await
+                                .unwrap_or_else(|_| "<unable to read body>".to_string());
+
                             warn!(
                                 "Request {}/{} failed - Server error: {}",
-                                i + 1,
-                                max_retries,
-                                response
-                                    .body_string()
-                                    .await
-                                    .unwrap_or_else(|_| "<unable to read body>".to_string())
+                                i, max_retries, response_body
                             );
                             sleep(error_wait_time).await;
                             continue;
@@ -109,7 +109,7 @@ async fn retriable_request<
                         return Ok(response);
                     }
                     Err(error) => {
-                        warn!("Request {}/{} failed: {}", i + 1, max_retries, error);
+                        warn!("Request {}/{} failed: {}", i, max_retries, error);
                         sleep(error_wait_time).await;
                         continue;
                     }
@@ -118,9 +118,7 @@ async fn retriable_request<
             Err(error) => {
                 warn!(
                     "Request {}/{} failed to be created: {}",
-                    i + 1,
-                    max_retries,
-                    error
+                    i, max_retries, error
                 );
                 sleep(error_wait_time).await;
                 continue;
@@ -532,6 +530,55 @@ mod tests {
     }
 
     #[async_std::test]
+    pub async fn upload_data_fails_on_failed_request() -> Result<()> {
+        let done_until = Arc::new(AtomicUsize::new(0));
+        let client = MockHttpClient {
+            config: Default::default(),
+            handle_request: Box::new(|mut req| {
+                let body = block_on(req.body_bytes()).unwrap();
+                assert_eq!(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9], body);
+                assert_eq!(req.header("Content-Range").unwrap()[0], "bytes 0-9/*");
+
+                let mut response = Response::new(StatusCode::Ok);
+                response.insert_header("Range", "bytes=0-10");
+                Ok(response)
+            }),
+            simulate_failure: Arc::new(AtomicBool::new(true)),
+            simulate_transport_failure: Arc::new(AtomicBool::new(true)),
+        };
+        let mut sessions_per_file = HashMap::new();
+        sessions_per_file.insert(
+            FileId::new("bucket", "my_file"),
+            url::Url::parse("https://example.com/session").unwrap(),
+        );
+        let mut api_client = DefaultApiClient {
+            sessions_per_file,
+            client,
+            backoff_strategy: ExponentialBackoffRetryStrategy {
+                max_retries: 1,
+                base_sleep_time: Duration::from_nanos(1),
+            },
+        };
+        let result = api_client
+            .handle_http_command(
+                done_until,
+                &Url::parse("http://example.com/upload").unwrap(),
+                HttpTaskCommand::UploadData {
+                    file: FileId::new("bucket", "my_file"),
+                    data: BufferPart {
+                        data: vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                        start: 0,
+                    },
+                },
+            )
+            .await;
+
+        assert!(result.is_err());
+
+        Ok(())
+    }
+
+    #[async_std::test]
     pub async fn can_finish_upload() -> Result<()> {
         let done_until = Arc::new(AtomicUsize::new(0));
         let client = MockHttpClient {
@@ -600,6 +647,30 @@ mod tests {
 
         assert!(request_handled.load(Ordering::Acquire));
         assert_eq!(response.status(), StatusCode::Ok);
+    }
+
+    #[async_std::test]
+    async fn fails_when_retries_are_exhausted() {
+        let request_handled = Arc::new(AtomicBool::new(false));
+        let request_handled_clone = request_handled.clone();
+
+        let response = retriable_request(
+            &ExponentialBackoffRetryStrategy::new(1, Duration::from_nanos(1)),
+            &mut MockHttpClient {
+                config: Default::default(),
+                handle_request: Box::new(move |_req| {
+                    request_handled_clone.swap(true, Ordering::Acquire);
+
+                    Ok(Response::new(StatusCode::Ok))
+                }),
+                simulate_failure: Arc::new(AtomicBool::new(true)),
+                simulate_transport_failure: Arc::new(Default::default()),
+            },
+            || Ok(Request::new(Method::Get, "http://example.com")),
+        )
+        .await;
+
+        assert!(response.is_err());
     }
 
     #[async_std::test]
