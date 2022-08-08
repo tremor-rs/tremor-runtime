1791cb990f1100c6f42ce133f15369988f1446fa
Matthias Wahl <matthiaswahl@m7w3.de>
2022-08-08 11:23:20 +0200

    Fix some edge conditions in failing and acking kafka messages in the consumer.
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- src/connectors/tests/kafka/consumer.rs
+++ src/connectors/tests/kafka/consumer.rs
@@ -19,8 +19,12 @@ use crate::{
     errors::Result,
 };
 use async_std::task;
+use async_std::prelude::FutureExt;
 use beef::Cow;
+use std::collections::HashMap;
 use rdkafka::{
+    Offset, error::KafkaResult,
+    consumer::{BaseConsumer, Consumer},
     admin::{AdminClient, AdminOptions, NewTopic, TopicReplication},
     config::FromClientConfig,
     message::OwnedHeaders,
@@ -36,7 +40,7 @@ use value_trait::Builder;
 
 #[async_std::test]
 #[serial(kafka)]
-async fn connector_kafka_consumer_transactional_retry() -> Result<()> {
+async fn transactional_retry() -> Result<()> {
     serial_test::set_max_wait(Duration::from_secs(600));
 
     let _ = env_logger::try_init();
@@ -248,7 +252,7 @@ async fn connector_kafka_consumer_transactional_retry() -> Result<()> {
     assert_eq!(
         &literal!({
             "error": "SIMD JSON error: InternalError at character 0 ('}')",
-            "source": "test::connector_kafka_consumer_transactional_retry",
+            "source": "test::transactional_retry",
             "stream_id": 8589934592_u64,
             "pull_id": 1u64
         }),
@@ -272,14 +276,41 @@ async fn connector_kafka_consumer_transactional_retry() -> Result<()> {
     let (out_events, err_events) = harness.stop().await?;
     assert!(out_events.is_empty());
     assert!(err_events.is_empty());
+
+    // check out the committed offsets with another consumer
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(1)));
+    assert_eq!(offsets.get(&(topic.to_string(), 1)), Some(&Offset::Offset(1)));
+    assert_eq!(offsets.get(&(topic.to_string(), 2)), Some(&Offset::Invalid)); // nothing committed yet
+    
+
     // cleanup
     drop(container);
     Ok(())
 }
 
+async fn get_offsets(broker: &str, group_id: &str, topic: &str) -> KafkaResult<HashMap<(String, i32), Offset>> {
+    let consumer: BaseConsumer = ClientConfig::new()
+        .set("bootstrap.servers", broker)
+        .set("group.id", group_id)
+        .create().expect("Error creating consumer");
+    consumer.subscribe(&[topic])?;
+    let mut assignment = consumer.assignment()?;
+    while assignment.count() == 0 {
+        task::sleep(Duration::from_millis(100)).await;
+        consumer.poll(Duration::ZERO);
+        assignment = consumer.assignment()?;
+    }
+    drop(assignment);
+    
+    let offsets = consumer.committed(Duration::from_secs(5))?.to_topic_map();
+    drop(consumer);
+    Ok(offsets)
+}
+
 #[async_std::test]
 #[serial(kafka)]
-async fn connector_kafka_consumer_transactional_no_retry() -> Result<()> {
+async fn transactional_no_retry() -> Result<()> {
     serial_test::set_max_wait(Duration::from_secs(600));
 
     let _ = env_logger::try_init();
@@ -291,6 +322,7 @@ async fn connector_kafka_consumer_transactional_no_retry() -> Result<()> {
     let mut admin_config = ClientConfig::new();
     let broker = format!("127.0.0.1:{}", port);
     let topic = "tremor_test_no_retry";
+    let group_id = "test1";
     admin_config
         .set("client.id", "test-admin")
         .set("bootstrap.servers", &broker);
@@ -325,7 +357,7 @@ async fn connector_kafka_consumer_transactional_no_retry() -> Result<()> {
             "brokers": [
                 broker.clone()
             ],
-            "group_id": "test1",
+            "group_id": group_id,
             "topics": [
                 topic
             ],
@@ -480,7 +512,7 @@ async fn connector_kafka_consumer_transactional_no_retry() -> Result<()> {
     assert_eq!(
         &literal!({
             "error": "SIMD JSON error: InternalError at character 0 ('}')",
-            "source": "test::connector_kafka_consumer_transactional_no_retry",
+            "source": "test::transactional_no_retry",
             "stream_id": 8589934592_u64,
             "pull_id": 1u64
         }),
@@ -504,6 +536,11 @@ async fn connector_kafka_consumer_transactional_no_retry() -> Result<()> {
     let (out_events, err_events) = harness.stop().await?;
     assert!(out_events.is_empty());
     assert!(err_events.is_empty());
+
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Invalid));
+    assert_eq!(offsets.get(&(topic.to_string(), 1)), Some(&Offset::Offset(1)));
+    assert_eq!(offsets.get(&(topic.to_string(), 2)), Some(&Offset::Invalid));
     // cleanup
     drop(container);
     Ok(())
@@ -511,7 +548,7 @@ async fn connector_kafka_consumer_transactional_no_retry() -> Result<()> {
 
 #[async_std::test]
 #[serial(kafka)]
-async fn connector_kafka_consumer_non_transactional() -> Result<()> {
+async fn performance() -> Result<()> {
     serial_test::set_max_wait(Duration::from_secs(600));
 
     let _ = env_logger::try_init();
@@ -706,7 +743,7 @@ async fn connector_kafka_consumer_non_transactional() -> Result<()> {
     assert_eq!(
         &literal!({
             "error": "SIMD JSON error: InternalError at character 0 ('}')",
-            "source": "test::connector_kafka_consumer_non_transactional",
+            "source": "test::performance",
             "stream_id": 8589934592_u64,
             "pull_id": 1u64
         }),
@@ -730,6 +767,12 @@ async fn connector_kafka_consumer_non_transactional() -> Result<()> {
     let (out_events, err_events) = harness.stop().await?;
     assert!(out_events.is_empty());
     assert!(err_events.is_empty());
+
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(1)));
+    assert_eq!(offsets.get(&(topic.to_string(), 1)), Some(&Offset::Offset(1)));
+    assert_eq!(offsets.get(&(topic.to_string(), 2)), Some(&Offset::Offset(2)));
+
     // cleanup
     drop(container);
     Ok(())
@@ -899,4 +942,392 @@ async fn connector_kafka_consumer_pause_resume() -> Result<()> {
     // cleanup
     drop(container);
     Ok(())
+}
+
+
+#[async_std::test]
+#[serial(kafka)]
+async fn transactional_store_offset_handling() -> Result<()> {
+    serial_test::set_max_wait(Duration::from_secs(600));
+
+    let _ = env_logger::try_init();
+
+    let docker = DockerCli::default();
+    let container = redpanda_container(&docker).await?;
+
+    let port = container.get_host_port_ipv4(9092);
+    let mut admin_config = ClientConfig::new();
+
+    let broker = format!("127.0.0.1:{}", port);
+    let topic = "tremor_test_store_offsets";
+    let group_id = "group_transactional_store_offsets";
+
+    admin_config
+        .set("client.id", "test-admin")
+        .set("bootstrap.servers", &broker);
+    let admin_client = AdminClient::from_config(&admin_config)?;
+    let options = AdminOptions::default();
+    let res = admin_client
+        .create_topics(
+            vec![&NewTopic::new(topic, 1, TopicReplication::Fixed(1))],
+            &options,
+        )
+        .await?;
+    for r in res {
+        match r {
+            Err((topic, err)) => {
+                error!("Error creating topic {}: {}", &topic, err);
+            }
+            Ok(topic) => {
+                info!("Created topic {}", topic);
+            }
+        }
+    }
+    let producer: BaseProducer = ClientConfig::new()
+        .set("bootstrap.servers", &broker)
+        .create()
+        .expect("Producer creation error");
+    let commit_interval: u64 = Duration::from_millis(100).as_nanos().try_into()?;
+    let connector_config = literal!({
+        "codec": "json-sorted",
+        "config": {
+            "brokers": [
+                broker.clone()
+            ],
+            "group_id": group_id,
+            "topics": [
+                topic
+            ],
+            "mode": {
+                "transactional": {
+                    "commit_interval": commit_interval
+                }
+            }
+        }
+    });
+    let harness = ConnectorHarness::new(
+        function_name!(),
+        &kafka::consumer::Builder::default(),
+        &connector_config,
+    )
+    .await?;
+    let out = harness.out().expect("No pipe connected to port OUT");
+    harness.start().await?;
+    harness.wait_for_connected().await?;
+
+    task::sleep(Duration::from_secs(5)).await;
+
+    // send message 1
+    let record1 = BaseRecord::to(topic)
+        .key("1")
+        .payload("1")
+        .partition(0)
+        .timestamp(1);
+    if producer.send(record1).is_err() {
+        return Err("Unable to send record to Kafka".into());
+    }
+    // send message 2
+    let record2 = BaseRecord::to(topic)
+        .key("2")
+        .payload("2")
+        .partition(0)
+        .timestamp(2);
+    if producer.send(record2).is_err() {
+        return Err("Unable to send record to Kafka".into());
+    }
+    producer.flush(Duration::from_secs(1));
+
+    // send message 3
+    let record3 = BaseRecord::to(topic)
+        .key("3")
+        .payload("3")
+        .partition(0)
+        .timestamp(3);
+    if producer.send(record3).is_err() {
+        return Err("Unable to send record to Kafka".into());
+    }
+    producer.flush(Duration::from_secs(1));
+
+    // receive events
+    let event1 = out.get_event().await?;
+    assert_eq!(&Value::from(1), event1.data.suffix().value());
+    let event2 = out.get_event().await?;
+    assert_eq!(&Value::from(2), event2.data.suffix().value());
+    let event3 = out.get_event().await?;
+    assert_eq!(&Value::from(3), event3.data.suffix().value());
+
+    // ack message 3
+    harness
+        .send_contraflow(CbAction::Ack, event3.id.clone())
+        .await?;
+    // ack message 1
+    harness
+        .send_contraflow(CbAction::Ack, event1.id.clone())
+        .await?;
+
+    // stop harness
+    let (out_events, err_events) = harness.stop().await?;
+    assert!(out_events.is_empty());
+    assert!(err_events.is_empty());
+
+    debug!("getting offsets...");
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(3)));
+
+    debug!("before start");
+    // start new harness
+    let harness = ConnectorHarness::new(
+        function_name!(),
+        &kafka::consumer::Builder::default(),
+        &connector_config,
+    )
+    .await?;
+    harness.start().await?;
+    harness.wait_for_connected().await?;
+    debug!("connected");
+
+    // fail message 2
+    harness.send_contraflow(CbAction::Fail, event2.id.clone()).await?;
+
+    debug!("failed event2");
+
+    // stop harness
+    let _ = harness.stop().await?;
+
+    // check that fail did reset the offset correctly
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(1)));
+    drop(offsets);
+
+    // start new harness
+    let harness = ConnectorHarness::new(
+        function_name!(),
+        &kafka::consumer::Builder::default(),
+        &connector_config,
+    )
+    .await?;
+    let out = harness.out().expect("No pipe connected to port OUT");
+    harness.start().await?;
+    harness.wait_for_connected().await?;
+
+    // ensure message 2 and 3 are received
+    // for some strange reason rdkafka might consumer event2 twice
+    let mut event_again = out.get_event().await?;
+    let mut data = event_again.data.suffix().value();
+    while data == &Value::from(2) {
+        event_again = out.get_event().await?;
+        data = event_again.data.suffix().value();
+    }
+    assert_eq!(&Value::from(3), data);
+    
+    // ack message 3 only
+    harness.send_contraflow(CbAction::Ack, event_again.id.clone()).await?;
+
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(3)));
+
+    // stop harness
+    let _ = harness.stop().await?;
+
+    drop(container);
+    Ok(())
+}
+
+#[async_std::test]
+#[serial(kafka)]
+async fn transactional_commit_offset_handling() -> Result<()> {
+    serial_test::set_max_wait(Duration::from_secs(600));
+
+    let _ = env_logger::try_init();
+
+    let docker = DockerCli::default();
+    let container = redpanda_container(&docker).await?;
+
+    let port = container.get_host_port_ipv4(9092);
+    let mut admin_config = ClientConfig::new();
+
+    let broker = format!("127.0.0.1:{}", port);
+    let topic = "tremor_test_commit_offset";
+    let group_id = "group_commit_offset";
+
+    admin_config
+        .set("client.id", "test-admin")
+        .set("bootstrap.servers", &broker);
+    let admin_client = AdminClient::from_config(&admin_config)?;
+    let options = AdminOptions::default();
+    let res = admin_client
+        .create_topics(
+            vec![&NewTopic::new(topic, 1, TopicReplication::Fixed(1))],
+            &options,
+        )
+        .await?;
+    for r in res {
+        match r {
+            Err((topic, err)) => {
+                error!("Error creating topic {}: {}", &topic, err);
+            }
+            Ok(topic) => {
+                info!("Created topic {}", topic);
+            }
+        }
+    }
+    let producer: BaseProducer = ClientConfig::new()
+        .set("bootstrap.servers", &broker)
+        .create()
+        .expect("Producer creation error");
+    let connector_config = literal!({
+        "codec": "json-sorted",
+        "config": {
+            "brokers": [
+                broker.clone()
+            ],
+            "group_id": group_id,
+            "topics": [
+                topic
+            ],
+            "mode": {
+                // "custom": {
+                //     "retry_failed_events": true,
+                //     "rdkafka_options": {
+                //         "enable.auto.commit": "true",
+                //         "enable.auto.offset.store": "false",
+                //         "debug": "all"
+                //     }
+                // }
+                "transactional": {
+                   "commit_interval": 0 // trigger direct commits
+                }
+            }
+        }
+    });
+    let harness = ConnectorHarness::new(
+        function_name!(),
+        &kafka::consumer::Builder::default(),
+        &connector_config,
+    )
+    .await?;
+    let out = harness.out().expect("No pipe connected to port OUT");
+    harness.start().await?;
+    harness.wait_for_connected().await?;
+
+    task::sleep(Duration::from_secs(5)).await;
+
+    // send message 1
+    let record1 = BaseRecord::to(topic)
+        .key("1")
+        .payload("1")
+        .partition(0)
+        .timestamp(1);
+    if producer.send(record1).is_err() {
+        return Err("Unable to send record to Kafka".into());
+    }
+    // send message 2
+    let record2 = BaseRecord::to(topic)
+        .key("2")
+        .payload("2")
+        .partition(0)
+        .timestamp(2);
+    if producer.send(record2).is_err() {
+        return Err("Unable to send record to Kafka".into());
+    }
+    producer.flush(Duration::from_secs(1));
+
+    // send message 3
+    let record3 = BaseRecord::to(topic)
+        .key("3")
+        .payload("3")
+        .partition(0)
+        .timestamp(3);
+    if producer.send(record3).is_err() {
+        return Err("Unable to send record to Kafka".into());
+    }
+    producer.flush(Duration::from_secs(1));
+
+    // receive events
+    let event1 = out.get_event().await?;
+    assert_eq!(&Value::from(1), event1.data.suffix().value());
+    let event2 = out.get_event().await?;
+    assert_eq!(&Value::from(2), event2.data.suffix().value());
+    let event3 = out.get_event().await?;
+    assert_eq!(&Value::from(3), event3.data.suffix().value());
+
+    // ack message 3
+    harness
+        .send_contraflow(CbAction::Ack, event3.id.clone())
+        .await?;
+    // ack message 1
+    harness
+        .send_contraflow(CbAction::Ack, event1.id.clone())
+        .await?;
+
+    // stop harness
+    let (out_events, err_events) = harness.stop().await?;
+    assert!(out_events.is_empty());
+    assert!(err_events.is_empty());
+
+
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(3)));
+
+    // start new harness
+    let harness = ConnectorHarness::new(
+        function_name!(),
+        &kafka::consumer::Builder::default(),
+        &connector_config,
+    )
+    .await?;
+    let out = harness.out().expect("No pipe connected to port OUT");
+    harness.start().await?;
+    harness.wait_for_connected().await?;
+
+    // ensure no message is received
+    assert!(out.get_event().timeout(Duration::from_millis(200)).await.is_err());
+
+    // fail message 2
+    harness.send_contraflow(CbAction::Fail, event2.id.clone()).await?;
+    
+    // ensure message 2 is the next - seek was effective
+    let event2_again = out.get_event().await?;
+    assert_eq!(&Value::from(2), event2_again.data.suffix().value());
+
+    // stop harness
+    let _ = harness.stop().await?;
+
+    // check that fail did reset the offset correctly - commit was effective
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(1)));
+
+    // start new harness
+    let harness = ConnectorHarness::new(
+        function_name!(),
+        &kafka::consumer::Builder::default(),
+        &connector_config,
+    )
+    .await?;
+    let out = harness.out().expect("No pipe connected to port OUT");
+    harness.start().await?;
+    harness.wait_for_connected().await?;
+
+    // ensure message 2 and 3 are received
+    // for some strange reason rdkafka might consumer event2 twice
+    let mut event_again = out.get_event().await?;
+    let mut data = event_again.data.suffix().value();
+    while data == &Value::from(2) {
+        event_again = out.get_event().await?;
+        data = event_again.data.suffix().value();
+    }
+    assert_eq!(&Value::from(3), data);
+
+    // ack message 3 only
+    harness.send_contraflow(CbAction::Ack, event_again.id.clone()).await?;
+
+    // stop harness
+    let _ = harness.stop().await?;
+
+    let offsets = get_offsets(broker.as_str(), group_id, topic).await?;
+    assert_eq!(offsets.get(&(topic.to_string(), 0)), Some(&Offset::Offset(3)));
+
+    drop(container);
+    Ok(())
+    
 }
