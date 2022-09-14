932da448f12dd39fe54756bc3b53d4cd65c3d38a
Matthias Wahl <matthiaswahl@m7w3.de>
2022-09-14 13:03:02 +0200

    [gsub_consumer] check that subscription exists directly in connect
    and remove wrong no_token test that didn't work because it was never getting a token from gouth in the first place.
    
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- src/connectors/tests/gpubsub/gsub.rs
+++ src/connectors/tests/gpubsub/gsub.rs
@@ -14,12 +14,10 @@
 
 use crate::connectors::impls::gpubsub::consumer::Builder;
 use crate::connectors::tests::ConnectorHarness;
-use crate::connectors::utils::EnvHelper;
 use crate::errors::Result;
-use crate::instance::State;
 use googapis::google::pubsub::v1::publisher_client::PublisherClient;
 use googapis::google::pubsub::v1::subscriber_client::SubscriberClient;
-use googapis::google::pubsub::v1::{PublishRequest, PubsubMessage, Subscription, Topic};
+use googapis::google::pubsub::v1::{PublishRequest, PubsubMessage, Subscription, Topic, GetSubscriptionRequest};
 use serial_test::serial;
 use std::collections::HashMap;
 use testcontainers::clients::Cli;
@@ -49,27 +47,42 @@ async fn no_connection() -> Result<()> {
     Ok(())
 }
 
-#[async_std::test]
-#[serial(gpubsub, timeout_ms = 600000)]
-async fn no_token() -> Result<()> {
-    let _ = env_logger::try_init();
-    let mut env = EnvHelper::new();
-    env.remove_var("GOOGLE_APPLICATION_CREDENTIALS");
-    let connector_yaml = literal!({
-        "codec": "binary",
-        "config":{
-            "ack_deadline": 30000000000u64,
-            "connect_timeout": 100000000,
-            "subscription_id": "projects/xxx/subscriptions/test-subscription-a"
-        }
-    });
+async fn create_subscription(endpoint: String, topic: &str, subscription: &str) -> Result<()> {
+    let channel = Channel::from_shared(endpoint)?.connect().await?;
+    let mut publisher = PublisherClient::new(channel.clone());
+    publisher
+        .create_topic(Topic {
+            name: topic.to_string(),
+            labels: Default::default(),
+            message_storage_policy: None,
+            kms_key_name: "".to_string(),
+            schema_settings: None,
+            satisfies_pzs: false,
+            message_retention_duration: None,
+        })
+        .await?;
 
-    let harness =
-        ConnectorHarness::new(function_name!(), &Builder::default(), &connector_yaml).await?;
-
-    harness.consume_initial_sink_contraflow().await?;
-    harness.wait_for_state(State::Failed).await?;
-
+    let mut subscriber = SubscriberClient::new(channel);
+    subscriber
+        .create_subscription(Subscription {
+            name: subscription.to_string(),
+            topic: topic.to_string(),
+            push_config: None,
+            ack_deadline_seconds: 0,
+            retain_acked_messages: false,
+            message_retention_duration: None,
+            labels: Default::default(),
+            enable_message_ordering: false,
+            expiration_policy: None,
+            filter: "".to_string(),
+            dead_letter_policy: None,
+            retry_policy: None,
+            detached: false,
+            topic_message_retention_duration: None,
+        })
+        .await?;
+    // assert the system knows about our subscription now
+    dbg!(subscriber.get_subscription(GetSubscriptionRequest { subscription: subscription.to_string() }).await?);
     Ok(())
 }
 
@@ -88,53 +101,20 @@ async fn simple_subscribe() -> Result<()> {
     let port = container
         .get_host_port_ipv4(testcontainers::images::google_cloud_sdk_emulators::PUBSUB_PORT);
     let endpoint = format!("http://localhost:{}", port);
-    let endpoint_clone = endpoint.clone();
+    let topic = "projects/test/topics/test";
+    let subscription = "projects/test/subscriptions/test-subscription-a";
 
     let connector_yaml: Value = literal!({
         "metrics_interval_s": 1,
         "codec": "binary",
         "config":{
-            "url": endpoint,
+            "url": endpoint.clone(),
             "ack_deadline": 30000000000u64,
             "connect_timeout": 30000000000u64,
-            "subscription_id": "projects/test/subscriptions/test-subscription-a",
-            "skip_authentication": true
+            "subscription_id": subscription,
         }
     });
-
-    let channel = Channel::from_shared(endpoint_clone)?.connect().await?;
-    let mut publisher = PublisherClient::new(channel.clone());
-    publisher
-        .create_topic(Topic {
-            name: "projects/test/topics/test".to_string(),
-            labels: Default::default(),
-            message_storage_policy: None,
-            kms_key_name: "".to_string(),
-            schema_settings: None,
-            satisfies_pzs: false,
-            message_retention_duration: None,
-        })
-        .await?;
-
-    let mut subscriber = SubscriberClient::new(channel);
-    subscriber
-        .create_subscription(Subscription {
-            name: "projects/test/subscriptions/test-subscription-a".to_string(),
-            topic: "projects/test/topics/test".to_string(),
-            push_config: None,
-            ack_deadline_seconds: 0,
-            retain_acked_messages: false,
-            message_retention_duration: None,
-            labels: Default::default(),
-            enable_message_ordering: false,
-            expiration_policy: None,
-            filter: "".to_string(),
-            dead_letter_policy: None,
-            retry_policy: None,
-            detached: false,
-            topic_message_retention_duration: None,
-        })
-        .await?;
+    create_subscription(endpoint.clone(), topic, subscription).await?;
 
     let harness =
         ConnectorHarness::new(function_name!(), &Builder::default(), &connector_yaml).await?;
@@ -148,7 +128,9 @@ async fn simple_subscribe() -> Result<()> {
 
     let mut attributes = HashMap::new();
     attributes.insert("a".to_string(), "b".to_string());
-
+    
+    let channel = Channel::from_shared(endpoint)?.connect().await?;
+    let mut publisher = PublisherClient::new(channel.clone());
     publisher
         .publish(PublishRequest {
             topic: "projects/test/topics/test".to_string(),
