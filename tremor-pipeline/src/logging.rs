6a0271f47a94ca4d1e5eef202aac0fe6b09886a3
Perle_nwar <rebeccaabli3@gmail.com>
2022-10-28 12:31:27 +0200

    Add support for named/pos+varargs fmt for logging
    > Update "tremor-pipeline/src/logging.rs"
    >> Add more capabilities to format logging messages. It now supports:
    >>> Named formatting via container (dictionnay/hashmap/object)
    >>> Positional formatting via container (array/list)
    >>> Positional formatting via var args
    >> Move the content of `TremorFn for PluggableLoggingFm :: invoke` to function `named_formatting(format: &str, arg_stack: &Value) -> FResult<Value>`
    >> Add new function `positional_formatting(format: &str, arg_stack: &Value) -> FResult<Value>`
    
    > Update "tests/queries/pluggable_logging/"
    >> Add a lot of test cases
    >>> Update "in" and "expected" consequently
    >> Update "query.troy" to take in account the diversity of tests
    >>> Flow added for varargs, but unused for now
    >>>> Add file (for now empty) "in_varargs"

--- tremor-pipeline/src/logging.rs
+++ tremor-pipeline/src/logging.rs
@@ -12,12 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use beef::Cow;
-
+//use beef::Cow;
+//use halfbrown::HashMap;
 use crate::{errors::Result};
-use halfbrown::HashMap;
 use simd_json::ValueAccess;
-//use simd_json::Builder;
 use tremor_script::{registry::{Registry, FResult, mfa, FunctionError}, TremorFn, EventContext, Value, TremorFnWrapper};
 /// Install's common functions into a registry
 ///
@@ -43,97 +41,36 @@ impl TremorFn for PluggableLoggingFm {
             .split_first()
             .and_then(|(f, arg_stack)| Some((f.as_str()?, arg_stack)))
         {
+			let out = if arg_stack.len() > 1 {
+				let arg_stack: Vec<Value> = arg_stack.iter().map(|v| v.clone_static()).collect();
+				positional_formatting(format, &Value::Array(arg_stack))
+			} else if arg_stack.len() == 1 {
+				let mut arg_stack: Vec<&&Value> = arg_stack.iter().collect();
+				let &arg_stack = arg_stack.pop().unwrap();
+				//todo manage when data is not object
+				if let Some(_) = arg_stack.as_object() { // retrieve format args as a hashmap
+					named_formatting(format, arg_stack)
+				} else if let Some(_) = arg_stack.as_array() {
+					positional_formatting(format, arg_stack)
+				} else {
+					positional_formatting(format, &Value::Array(vec![arg_stack.clone()]))
+				}
+			} else { // arg_stack.len() < 1 (== 0)
+				positional_formatting(format, &Value::Array(vec![])) // `arg_stack` is empty
+			};
 
-			let this_mfa = || mfa("logging", format, args.len()); // because we now know the function
-
-			let mut arg_stack: Vec<_> = arg_stack.iter().collect();
-			let mut format_fields = &HashMap::<Cow<str>, Value>::default();
-			if !arg_stack.is_empty() {
-				//tdo manage when data is not object
-				format_fields = match arg_stack.pop().unwrap().as_object() { // retrieve format args as a hashmap
-					Some(obj) => obj,
-					None => return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("wrong Tremor::Value given to contain format values: object expected, but another type was given (fyi arrays are supported, because it is not a positionnal formatting, but only named formatting)")})
-				};
-				if !arg_stack.is_empty() { // `pop()` should empty this supposedly array of length 1
-					warn!("Additional parameters given to logging function {format} will not be used");
+			match out {
+				Ok(out_ok) => {
+					info!("{}", out_ok.clone());
+					dbg!(out_ok.clone());
+					dbg!(args.clone());
+					Ok(out_ok)
+				},
+				Err(a) => {
+					Err(dbg!(a))
+					//out
 				}
 			}
-
-            let mut out = String::with_capacity(format.len());
-			// out.push('['); out.push_str(format); out.push(']');
-            let mut iter = format.chars().enumerate();
-            while let Some((pos, char)) = iter.next() {
-                match char {
-                    '\\' =>
-						if let Some((_, c)) = iter.next() {
-							if c != '{' && c != '}' {
-								out.push('\\');
-							};
-							out.push(c);
-						} else {
-							return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("bad escape sequence at {pos}: cannot end string with escape character")});
-						},
-                    '{' => {
-						let mut identifier = String::new(); // named placeholder "format specifier"
-						let mut init = true;
-						loop { // reading the identifier name
-							match iter.next() {
-								Some((_, '{')) => {
-									if init {
-										out.push('{');
-										break;
-									} else {
-										return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {pos} is invalid. You have to terminate `{{` with another `{{` to escape it")});
-									}
-								},
-								Some((_, '}')) => {
-									match format_fields.get(identifier.as_str()) {
-										Some(value) =>  {
-											out.push_str(format_tremor_value(value).unwrap().as_str());
-										},
-										None => return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format name at {pos} cannot be found among* the `Value:Object` given. Cannot insert field mapped with name {identifier} because it was not found in the given hashmap of args. *This is sus")})
-									}
-									break;
-								},
-								Some((_, c)) => {
-									init = false;
-									identifier.push(c);
-								},
-        						None => return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {pos} is invalid. Identifier truncated. No ending `}}` found.")})
-							}
-						}
-					}
-                    '}' =>
-						if let Some((_, '}')) = iter.next() {
-							out.push('}');
-						} else {
-							return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {pos} is invalid. You have to terminate `}}` with another `}}` to escape it")});
-						},
-                    c => out.push(c)
-                }
-				/*
-				return Err(FunctionError::RuntimeError{mfa: this_mfa(),
-					error: format!("the format specifier at {pos} is invalid. It only support named formatting, and not positional formatting.") +
-					&format!("You need to place an identifier to name the value to be retrieved and formatted")
-				});
-				//todo you are supposed to handle both formatting by value and positional formatting
-				//todo but in this case only positional formatting is handled
-				return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the arguments passed to the format function are less than the `{{}}` specifiers in the format string. The placeholder at {pos} can not be filled")});
-				*/
-            }
-            if !arg_stack.is_empty() {
-                return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: "too many parameters passed. Ensure that you have the same number of {} in your format string".into()});
-			}
-			dbg!(out.clone());
-			dbg!(args);
-
-			info!("{out}");
-			Ok(Value::from(out))
-			
-			//TODO add variable argument to sub-record
-			// Ok(Value::from("temp placeholder"))
-			//dbg!(_ctx.origin_uri().unwrap().to_string());
-
 		} else {
 			Err(FunctionError::RuntimeError{mfa: this_mfa(), error: "expected 1st parameter to format to be a format specifier e.g. to print a number use `string::format(\"{}\", 1)`".into()})
 		}
@@ -155,11 +92,155 @@ impl TremorFn for PluggableLoggingFm {
 }
 
 
-/// Take in account `Tremor::Value`s which can wrap String (&str), Integer (i32), Boolean (bool), Array (Vec), Bytes (bytes), Object (HashMap)
+/// Exact copy from String
+fn positional_formatting<'event>(format: &str, arg_stack: &Value) -> FResult<Value<'event>> {
+
+	let this_mfa = || mfa("logging", "TODO", 0); // because we now know the function
+
+	//let mut arg_stack = arg_stack.as_array().unwrap().clone();
+	let mut arg_stack: Vec<&Value> = arg_stack.as_array().unwrap().iter().rev().collect();
+
+	let mut out = String::with_capacity(format.len());
+	let mut iter = format.chars().enumerate();
+	while let Some((pos, char)) = iter.next() {
+		match char {
+			'\\' => if let Some((_, c)) = iter.next() {
+				if c != '{' && c != '}' {
+					out.push('\\');
+				};
+				out.push(c);
+			} else {
+				return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("bad escape sequence at {}", pos)});
+			}
+			'{' => match iter.next() {
+				Some((_, '}')) => if let Some(arg) = arg_stack.pop() {
+					out.push_str(&match format_tremor_value(arg) {
+						Ok(s) => s,
+						Err(e) => return Err(FunctionError::RuntimeError{ mfa: this_mfa(), error: e.to_string()})
+					});
+				} else {
+						return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the arguments passed to the format function are less than the `{{}}` specifiers in the format string. The placeholder at {} can not be filled", pos)});
+				},
+				Some((_, '{')) => {
+					out.push('{');
+				}
+				_ => {
+					return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {} is invalid. If you want to use `{{` as a literal in the string, you need to escape it with `{{{{`", pos)})
+				}
+			},
+			'}' => if let Some((_, '}')) =  iter.next() {
+				out.push('}');
+			} else {
+				return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {} is invalid. You have to terminate `}}` with another `}}` to escape it", pos)});
+			},
+			c => out.push(c),
+		}
+	}
+	// if arg_stack.is_empty() {
+	Ok(Value::from(out))
+	// } else {
+	// 	Err(FunctionError::RuntimeError{mfa: this_mfa(), error: "too many parameters passed. Ensure that you have the same number of {} in your format string".into()})
+	// }
+}
+
+
+fn named_formatting<'event>(format: &str, arg_stack: &Value) -> FResult<Value<'event>> {
+	let this_mfa = || mfa("logging", "TODO", 0); // because we now know the function
+
+	let format_fields = arg_stack.as_object().unwrap();
+
+	let mut out = String::with_capacity(format.len());
+	// out.push('['); out.push_str(format); out.push(']');
+	let mut iter = format.chars().enumerate();
+	while let Some((pos, char)) = iter.next() {
+		match char {
+			'\\' =>
+				if let Some((_, c)) = iter.next() {
+					if c != '{' && c != '}' {
+						out.push('\\');
+					};
+					out.push(c);
+				} else {
+					return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("bad escape sequence at {pos}: cannot end string with escape character")});
+				},
+			'{' => {
+				let mut identifier = String::new(); // named placeholder "format specifier"
+				let mut init = true;
+				loop { // reading the identifier name
+					match iter.next() {
+						Some((_, '{')) => {
+							if init {
+								out.push('{');
+								break;
+							} else {
+								return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {pos} is invalid. You have to terminate `{{` with another `{{` to escape it")});
+							}
+						},
+						Some((_, '}')) => {
+							match format_fields.get(identifier.as_str()) {
+								Some(value) =>  {
+									out.push_str(format_tremor_value(value).unwrap().as_str());
+								},
+								None => return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format name at {pos} cannot be found among* the `Value:Object` given. Cannot insert field mapped with name {identifier} because it was not found in the given hashmap of args. *This is sus")})
+							}
+							break;
+						},
+						Some((_, c)) => {
+							init = false;
+							identifier.push(c);
+						},
+						None => return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {pos} is invalid. Identifier truncated. No ending `}}` found.")})
+					}
+				}
+			}
+			'}' =>
+				if let Some((_, '}')) = iter.next() {
+					out.push('}');
+				} else {
+					return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the format specifier at {pos} is invalid. You have to terminate `}}` with another `}}` to escape it")});
+				},
+			c => out.push(c)
+		}
+		/*
+		return Err(FunctionError::RuntimeError{mfa: this_mfa(),
+			error: format!("the format specifier at {pos} is invalid. It only support named formatting, and not positional formatting.") +
+			&format!("You need to place an identifier to name the value to be retrieved and formatted")
+		});
+		//todo you are supposed to handle both formatting by value and positional formatting
+		//todo but in this case only positional formatting is handled
+		return Err(FunctionError::RuntimeError{mfa: this_mfa(), error: format!("the arguments passed to the format function are less than the `{{}}` specifiers in the format string. The placeholder at {pos} can not be filled")});
+		*/
+	}
+	Ok(Value::from(out))
+	
+	//TODO add variable argument to sub-record
+	// Ok(Value::from("temp placeholder"))
+	//dbg!(_ctx.origin_uri().unwrap().to_string());
+}
+
+
 fn format_tremor_value(value: &Value) -> Result<String> {
 	let mut result = String::new();
 	let error_result =
 	Err("Not Implemented. Could not deserialize from `Tremor::Value` to any supporteed rust types.\nSupported types are either &str, i32, bool, Vec, bytes, HashMap".into());
+
+	
+	//todo: use value.value_type() instead of trying to convert and fail
+	/*ValueType::Null;
+    ValueType::Bool;
+    ValueType::I64;
+    ValueType::I128;
+    ValueType::U64;
+    ValueType::U128;
+    ValueType::F64;
+	ValueType::String;
+	ValueType::Array;
+	ValueType::Object;
+    // ValueType::Extended(ExtendedValueType) // Extended are not supported as tremor value core types
+	ValueType::Custom("bytes");
+
+
+	*/
 
 	if let Some(string) = value.as_str() {
 		result.push_str(string);
