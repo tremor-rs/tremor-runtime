4d564a45e727899569217b9107f1a64d8fe8fbbf
Matthias Wahl <matthiaswahl@m7w3.de>
2022-11-03 14:10:59 +0100

    IMprove array::flatten to not allocate additional vecs
    but make it work with iterators
    
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>
    Signed-off-by: Ramona Łuczkiewicz <rluczkiewicz@wayfair.com>

--- tremor-script/src/std_lib/array.rs
+++ tremor-script/src/std_lib/array.rs
@@ -102,13 +102,17 @@ pub fn load(registry: &mut Registry) {
         }));
 }
 
-//TODO this is not very nice
-fn flatten_value<'event>(v: &Value<'event>) -> Vec<Value<'event>> {
-    if let Some(a) = v.as_array() {
-        a.iter().flat_map(flatten_value).collect()
-    } else {
-        vec![v.clone()]
+fn flatten_iter<'event, 'borrow>(
+    v: &'borrow Value<'event>,
+) -> Box<dyn Iterator<Item = &'borrow Value<'event>> + 'borrow> {
+    match v.array_iter() {
+        Some(iter) => Box::new(iter.flat_map(flatten_iter)),
+        None => Box::new(std::iter::once(v)),
     }
+}
+
+pub fn flatten_value<'event>(v: &Value<'event>) -> Vec<Value<'event>> {
+    flatten_iter(v).cloned().collect()
 }
 
 #[cfg(test)]
