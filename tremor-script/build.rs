002ce51f9eaf893db6128392f61d082e35b3b458
Matthias Wahl <matthiaswahl@m7w3.de>
2022-11-10 11:31:18 +0100

    Implement new std::datetime library
    Signed-off-by: Matthias Wahl <matthiaswahl@m7w3.de>

--- tremor-script/build.rs
+++ tremor-script/build.rs
@@ -13,6 +13,47 @@
 // limitations under the License.
 
 extern crate lalrpop;
+use chrono_tz::TZ_VARIANTS;
+use std::{io::Write, path::PathBuf};
+
+const TIMEZONE_DOCS: &'static str = r#"### Timezones extracted from TZ data
+### Never use the actual values, they are going to change.
+###
+### Usage:
+###
+### ```tremor
+### use std::datetime;
+### let date_str = datetime::format(datetime::with_timezone(0, datetime::timezones::EUROPE_BERLIN), datetime::formats::RFC3339);
+### ```
+"#;
+
+fn create_timezones_tremor() {
+    let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    dir.push("lib");
+    dir.push("std");
+    dir.push("datetime");
+    dir.push("timezones.tremor");
+    let mut file = std::fs::File::create(&dir).expect("Unable to create file timezones.tremor");
+    file.write_all(TIMEZONE_DOCS.as_bytes()).unwrap();
+
+    for (index, tz) in TZ_VARIANTS.iter().enumerate() {
+        let name = tz.name();
+        let ident_name = tz
+            .name()
+            .replace("GMT+", "GMT_PLUS_")
+            .replace("GMT-", "GMT_MINUS_")
+            .chars()
+            .map(|c| if c.is_alphanumeric() { c } else { '_' })
+            .map(|c| c.to_ascii_uppercase())
+            .collect::<String>();
+        let docstring = format!("## Timezone name constant for {name}\n");
+        let line = format!("const {ident_name} = {index};\n");
+        file.write_all(docstring.as_bytes())
+            .expect("expected writing to timezones.tremor to succeed");
+        file.write_all(line.as_bytes())
+            .expect("expected writing to timezones.tremor to succeed");
+    }
+}
 
 fn main() {
     lalrpop::Configuration::new()
@@ -22,4 +63,6 @@ fn main() {
 
     println!("cargo:rustc-cfg=can_join_spans");
     println!("cargo:rustc-cfg=can_show_location_of_runtime_parse_error");
+
+    create_timezones_tremor();
 }
